8feb5ceef3a2515810b8a054b7ef6046f96d9021
Talha <mdtalha2008@gmail.com>
2024-03-29 17:51:23 +0600

    motion add

--- src/pages/Home/Offer/Offer.jsx
+++ src/pages/Home/Offer/Offer.jsx
@@ -5,15 +5,15 @@ import { motion } from "framer-motion";
 const Offer = () => {
   return (
     <div>
-      <div className="flex flex-col md:flex-row md:flex-wrap justify-center items-center">
+      <div className="flex flex-col md:flex-row md:flex-wrap justify-center items-center ">
         <motion.div
           whileHover={{ scale: 1.05 }}
           whileTap={{ scale: 0.8 }}
-          className="card bg-base-100 shadow-xl mx-4 mb-8 md:w-96"
+          className="card bg-base-100 shadow-xl mx-4 mb-8 md:w-96 rounded"
         >
           <figure className="px-4 pt-10">
             <img
-              src="https://i.ibb.co/pxTDWWp/khela.webp"
+              src="https://i.ibb.co/6NWH8LP/Untitled-design-7.webp"
               alt="Shoes"
               className="rounded-xl"
             />
@@ -26,13 +26,20 @@ const Offer = () => {
             </p>
             <div className="card-actions card-actions flex justify-center items-center">
               <Link to="/cricket">
-                <button className="btn btn-outline w-full bg-black text-white border-black border group hover:bg-white hover:text-black">
-                  Read More
+                <motion.button 
+                initial={{x:-1000}}
+                animate={{x:0}}
+                transition={{
+                    duration:"2",
+                    delay:"0.2"
+                }}
+                className="btn btn-outline w-full bg-black text-white border-black border group hover:bg-white hover:text-black">
+                  Learn More
                   <PiArrowRightLight
                     size="1.1rem"
                     className="text-white group-hover:text-black"
                   />
-                </button>
+                </motion.button>
               </Link>
             </div>
           </div>
@@ -41,11 +48,11 @@ const Offer = () => {
         <motion.div
           whileHover={{ scale: 1.05 }}
           whileTap={{ scale: 0.8 }}
-          className="card bg-base-100 shadow-xl mx-4 mb-8 md:w-96"
+          className="card bg-base-100 shadow-xl mx-4 mb-8 md:w-96 rounded"
         >
           <figure className="px-4 pt-10">
             <img
-              src="https://i.ibb.co/pxTDWWp/khela.webp"
+              src="https://i.ibb.co/L0SHvMw/Untitled-design-6.webp"
               alt="Shoes"
               className="rounded-xl"
             />
@@ -58,13 +65,20 @@ const Offer = () => {
             </p>
             <div className="card-actions card-actions flex justify-center items-center">
               <Link to="/cricket">
-                <button className="btn btn-outline w-full bg-black text-white border-black border group hover:bg-white hover:text-black">
-                  Read More
+              <motion.button 
+                initial={{x:-1000}}
+                animate={{x:0}}
+                transition={{
+                    duration:"2",
+                    delay:"0.2"
+                }}
+                className="btn btn-outline w-full bg-black text-white border-black border group hover:bg-white hover:text-black">
+                  Learn More
                   <PiArrowRightLight
                     size="1.1rem"
                     className="text-white group-hover:text-black"
                   />
-                </button>
+                </motion.button>
               </Link>
             </div>
           </div>
@@ -73,11 +87,11 @@ const Offer = () => {
         <motion.div
           whileHover={{ scale: 1.05 }}
           whileTap={{ scale: 0.8 }}
-          className="card bg-base-100 shadow-xl mx-4 mb-8 md:w-96"
+          className="card bg-base-100 shadow-xl mx-4 mb-8 md:w-96 rounded"
         >
           <figure className="px-4 pt-10">
             <img
-              src="https://i.ibb.co/pxTDWWp/khela.webp"
+              src="https://i.ibb.co/jzQ7JGv/Untitled-design-8.webp"
               alt="Shoes"
               className="rounded-xl"
             />
@@ -91,13 +105,20 @@ const Offer = () => {
             </p>
             <div className="card-actions card-actions flex justify-center items-center">
               <Link to="/cricket">
-                <button className="btn btn-outline w-full bg-black text-white border-black border group hover:bg-white hover:text-black">
-                  Read More
+              <motion.button 
+                initial={{x:-1000}}
+                animate={{x:0}}
+                transition={{
+                    duration:"2",
+                    delay:"0.2"
+                }}
+                className="btn btn-outline w-full bg-black text-white border-black border group hover:bg-white hover:text-black">
+                  Learn More
                   <PiArrowRightLight
                     size="1.1rem"
                     className="text-white group-hover:text-black"
                   />
-                </button>
+                </motion.button>
               </Link>
             </div>
           </div>
