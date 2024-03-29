28b26b9a184e21c3740fe1f67eda9dad8447e943
Talha <mdtalha2008@gmail.com>
2024-03-29 16:31:16 +0600

    offer add

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import FAQ from '../FAQ/FAQ';
 import Technology from '../Technology/Technology';
+import Offer from '../Offer/Offer';
 
 const Home = () => {
     return (
         <div>
             <p>This is Home</p>
+            <Offer></Offer>
             <Technology></Technology>
             <FAQ></FAQ>
         </div>
--- src/pages/Home/Offer/Offer.jsx
+++ src/pages/Home/Offer/Offer.jsx
@@ -1,9 +1,94 @@
 import React from 'react';
-
+import { Link } from "react-router-dom";
+import { PiArrowRightLight } from "react-icons/pi";
 const Offer = () => {
     return (
         <div>
-            
+            <div className="flex flex-col md:flex-row md:flex-wrap justify-center items-center">
+        <div className="card bg-base-100 shadow-xl mx-4 mb-8 md:w-96">
+          <figure className="px-4 pt-10">
+            <img
+              src="https://i.ibb.co/pxTDWWp/khela.webp"
+              alt="Shoes"
+              className="rounded-xl"
+            />
+          </figure>
+          <div className="card-body text-center flex justify-center items-center">
+            <h2 className="card-title text-center">Programming Hero Seminar</h2>
+            <p>
+              Every year REC Sports club holds different types of sports
+              tournaments.
+            </p>
+            <div className="card-actions flex justify-center items-center">
+              <Link to="/programming-hero">
+                <button className="btn btn-outline w-full bg-black text-white border-black border group hover:bg-white hover:text-black">
+                  Read More
+                  <PiArrowRightLight
+                    size="1.1rem"
+                    className="text-white group-hover:text-black"
+                  />
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        <div className="card bg-base-100 shadow-xl mx-4 mb-8 md:w-96">
+          <figure className="px-4 pt-10">
+            <img
+              src="https://i.ibb.co/pxTDWWp/khela.webp"
+              alt="Shoes"
+              className="rounded-xl"
+            />
+          </figure>
+          <div className="card-body text-center flex justify-center items-center">
+            <h2 className="card-title">REC Cricket Tournament</h2>
+            <p>
+              Every year REC Sports club holds different types of sports
+              tournaments.
+            </p>
+            <div className="card-actions card-actions flex justify-center items-center">
+              <Link to="/cricket">
+                <button className="btn btn-outline w-full bg-black text-white border-black border group hover:bg-white hover:text-black">
+                  Read More
+                  <PiArrowRightLight
+                    size="1.1rem"
+                    className="text-white group-hover:text-black"
+                  />
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        <div className="card bg-base-100 shadow-xl mx-4 mb-8 md:w-96">
+          <figure className="px-4 pt-10">
+            <img
+              src="https://i.ibb.co/pxTDWWp/khela.webp"
+              alt="Shoes"
+              className="rounded-xl"
+            />
+          </figure>
+          <div className="card-body text-center flex justify-center items-center">
+            <h2 className="card-title">REC Cricket Tournament</h2>
+            <p>
+              Every year REC Sports club holds different types of sports
+              tournaments.
+            </p>
+            <div className="card-actions card-actions flex justify-center items-center">
+              <Link to="/cricket">
+                <button className="btn btn-outline w-full bg-black text-white border-black border group hover:bg-white hover:text-black">
+                  Read More
+                  <PiArrowRightLight
+                    size="1.1rem"
+                    className="text-white group-hover:text-black"
+                  />
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
         </div>
     );
 };
