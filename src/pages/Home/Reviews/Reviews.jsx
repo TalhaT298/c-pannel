18b523f782aa83c9c0acbdfa04a025a02a20d6a5
Talha <mdtalha2008@gmail.com>
2024-03-31 01:39:20 +0600

    background add

--- src/pages/Home/Reviews/Reviews.jsx
+++ src/pages/Home/Reviews/Reviews.jsx
@@ -14,11 +14,19 @@ const Reviews = () => {
     }, []);
 
     return (
-        <div className='mt-10 mb-20'>
-            <h3 className='py-7 text-3xl md:text-4xl font-bold text-yellow-900 text-center font-garamond'>Reviews of our Customers</h3>
-            <AwesomeSlider className='custom-slider'>
+        <div className='mt-10 mb-20 ' >
+            <div className="mx-auto md:w-8/12 my-4 flex justify-center items-center ">
+        {/* <p className='text-yellow-600'>----{subHeading}---</p> */}
+        <h3 className="text-4xl text-white uppercase  pt-4 font-bold font-serif">
+        What Our Client Say
+        </h3>
+      </div>
+      {/* <div className="h-1 w-[800px] bg-white rounded flex justify-center items-center mx-auto my-auto"></div> */}
+      {/* <div class="h-1 w-80 bg-white rounded flex justify-center items-center mx-auto my-auto animate-ping"></div> */}
+      <div class="h-1 w-[600px] bg-white rounded flex justify-center items-center mx-auto my-auto hover:animate-ping"></div>
+            <AwesomeSlider className='custom-slider' >
                 {reviews.map((userReview) => (
-                    <div key={userReview.id} className='review-slide'>
+                    <div key={userReview.id} className='review-slide' style={{ backgroundColor: 'black' }}>
                         <ReviewCard userReview={userReview} />
                     </div>
                 ))}
