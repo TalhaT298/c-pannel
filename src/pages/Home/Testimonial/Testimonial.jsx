c63f3f20f01645b1db5acf9c67659e52a108e7ee
Talha <mdtalha2008@gmail.com>
2024-03-31 00:51:34 +0600

    reviews.json add

--- src/pages/Home/Testimonial/Testimonial.jsx
+++ src/pages/Home/Testimonial/Testimonial.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import AwesomeSlider from 'react-awesome-slider';
+import 'react-awesome-slider/dist/styles.css';
+import './Testimonial.css';
+import { useEffect, useState } from 'react';
+import ReviewCard from './ReviewCard';
 
 const Testimonial = () => {
+    const [reviews, setReviews] = useState([]);
+
+    useEffect(() => {
+        fetch('/reviews.json')
+            .then((res) => res.json())
+            .then((data) => setReviews(data));
+    }, []);
+
     return (
-        <div>
-            
+        <div className='mt-10 mb-20'>
+            <h3 className='py-7 text-3xl md:text-4xl font-bold text-yellow-900 text-center font-garamond'>Reviews of our Customers</h3>
+            <AwesomeSlider className='custom-slider'>
+                {reviews.map((userReview) => (
+                    <div key={userReview.id} className='review-slide'>
+                        <ReviewCard userReview={userReview} />
+                    </div>
+                ))}
+            </AwesomeSlider>
         </div>
     );
 };
