37f55ad4ce3707790014a1d359071deac080238d
Talha <mdtalha2008@gmail.com>
2024-04-01 03:39:29 +0600

    div divided in idea

--- src/pages/Home/Idea/Idea.jsx
+++ src/pages/Home/Idea/Idea.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
-
+import React from "react";
+import idea from "../../../assets/idea.gif";
+import { Link } from "react-router-dom";
 const Idea = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    // <div class="relative inline-block">
+    //   <img src={idea} alt="" class="block"></img>
+    //   <div class="absolute pb-96 pl-60 transform -translate-x-1/2 -translate-y-1/2  bg-opacity-100 p-4">
+    //   Have Any Project Idea
+    //   </div>
+    // </div>
+    <div>
+        <div className="w-3/5"></div>
+        <div className="w-2/5"></div>
+    </div>
+  );
 };
 
 export default Idea;
