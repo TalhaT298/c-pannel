026427efe4911e2b6586c5a1d52fee08b863d85a
Talha <mdtalha2008@gmail.com>
2024-04-02 03:39:35 +0600

    cyber security add

---
+++ src/pages/CyberSecurity/CyberSecurity.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const CyberSecurity = () => {
+    return (
+        <Link>
+        <div>
+            this is cyber security
+            </div>
+        </Link>
+    );
+};
+
+export default CyberSecurity;
