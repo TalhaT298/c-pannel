e121340f1637cbeb1c186e749507612f44d174e1
Talha <mdtalha2008@gmail.com>
2024-04-01 16:55:47 +0600

    idea update

--- src/pages/Home/Idea/Idea.jsx
+++ src/pages/Home/Idea/Idea.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import idea from "../../../assets/idea.gif";
 import { Link } from "react-router-dom";
+import { PiArrowRightLight } from "react-icons/pi";
 const Idea = () => {
   return (
     // <div class="relative inline-block">
@@ -9,9 +10,27 @@ const Idea = () => {
     //   Have Any Project Idea
     //   </div>
     // </div>
-    <div>
-        <div className="w-3/5"></div>
-        <div className="w-2/5"></div>
+    <div className="flex">
+      <div className="w-3/5 pt-24 pl-20">
+        <h1 className="text-3xl font-bold pb-6">Have Any Project Idea</h1>
+        <p className="">
+          Lets discuss about your offshore project. We can Help you by our
+          skillful Global team. <br />
+          You can take a free counciling by taking a schedule
+        </p>
+        <Link to="">
+          <button className="btn btn-outline text-xl pl-1 mt-6 w- bg-black text-white border-black border group">
+            Get Started
+            <PiArrowRightLight
+              size="1.4rem"
+              className="text-white group-hover:text-black"
+            />
+          </button>
+        </Link>
+      </div>
+      <div className="w-2/5 pl-20">
+        <img src={idea} alt="" />
+      </div>
     </div>
   );
 };
