9d50f8e571ed108ee29b0b61102fbe54e392d5e0
Talha <mdtalha2008@gmail.com>
2024-04-02 03:45:14 +0600

    services add

---
+++ src/pages/Services/Services.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <Link to="/services">
+        <div>
+            
+            </div>
+        </Link>
+    );
+};
+
+export default Services;
