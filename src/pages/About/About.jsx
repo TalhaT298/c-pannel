f2c731064171ed3591cc586f30a11b6a531d94eb
Talha <mdtalha2008@gmail.com>
2024-04-02 03:52:48 +0600

    about at

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const About = () => {
+    return (
+        <>
+        <Link>
+        <div>
+            this is about
+        </div>
+        </Link>
+        </>
+    );
+};
+
+export default About;
