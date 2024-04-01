ac2a19b812e0b37976eccc374c5630c9247ca965
Talha <mdtalha2008@gmail.com>
2024-04-02 02:10:49 +0600

    recnt update

--- src/pages/Home/RecentArticle/RecentArticle.jsx
+++ src/pages/Home/RecentArticle/RecentArticle.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const RecentArticle = () => {
   return (
@@ -6,22 +7,22 @@ const RecentArticle = () => {
       <div className="mx-auto md:w-8/12 my-4 flex justify-center items-center ">
         {/* <p className='text-yellow-600'>----{subHeading}---</p> */}
         <h3 className="text-4xl text-white uppercase  pt-4 font-bold font-serif">
-        recent articles
+          recent articles
         </h3>
       </div>
       <div class="h-1 w-[600px] bg-white rounded flex justify-center items-center mx-auto my-auto hover:animate-ping"></div>
-      <section class="text-gray-600 body-font overflow-hidden">
+      <section class="text-white body-font overflow-hidden">
         <div class="container px-5 py-24 mx-auto">
           <div class="-my-8 divide-y-2 divide-gray-100">
             <div class="py-8 flex flex-wrap md:flex-nowrap">
               <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                <span class="font-semibold title-font text-gray-700">
+                <span class="font-semibold title-font text-white">
                   CATEGORY
                 </span>
-                <span class="mt-1 text-gray-500 text-sm">12 Jun 2019</span>
+                <span class="mt-1 text-white text-sm">12 Jun 2019</span>
               </div>
               <div class="md:flex-grow">
-                <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">
+                <h2 class="text-2xl font-medium text-white title-font mb-2">
                   Bitters hashtag waistcoat fashion axe chia unicorn
                 </h2>
                 <p class="leading-relaxed">
@@ -30,32 +31,34 @@ const RecentArticle = () => {
                   moon party messenger bag selfies, poke vaporware kombucha
                   lumbersexual pork belly polaroid hoodie portland craft beer.
                 </p>
-                <a class="text-indigo-500 inline-flex items-center mt-4">
-                  Learn More
-                  <svg
-                    class="w-4 h-4 ml-2"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    fill="none"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  >
-                    <path d="M5 12h14"></path>
-                    <path d="M12 5l7 7-7 7"></path>
-                  </svg>
-                </a>
+                <Link to="">
+                  <a class="text-white inline-flex items-center mt-4">
+                    Learn More
+                    <svg
+                      class="w-4 h-4 ml-2"
+                      viewBox="0 0 24 24"
+                      stroke="#ffffff"
+                      stroke-width="2"
+                      fill="none"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    >
+                      <path d="M5 12h14"></path>
+                      <path d="M12 5l7 7-7 7"></path>
+                    </svg>
+                  </a>
+                </Link>
               </div>
             </div>
             <div class="py-8 flex flex-wrap md:flex-nowrap">
               <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                <span class="font-semibold title-font text-gray-700">
+                <span class="font-semibold title-font text-white">
                   CATEGORY
                 </span>
-                <span class="mt-1 text-gray-500 text-sm">12 Jun 2019</span>
+                <span class="mt-1 text-white text-sm">12 Jun 2019</span>
               </div>
               <div class="md:flex-grow">
-                <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">
+                <h2 class="text-2xl font-medium text-white title-font mb-2">
                   Meditation bushwick direct trade taxidermy shaman
                 </h2>
                 <p class="leading-relaxed">
@@ -64,32 +67,34 @@ const RecentArticle = () => {
                   moon party messenger bag selfies, poke vaporware kombucha
                   lumbersexual pork belly polaroid hoodie portland craft beer.
                 </p>
-                <a class="text-indigo-500 inline-flex items-center mt-4">
-                  Learn More
-                  <svg
-                    class="w-4 h-4 ml-2"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    fill="none"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  >
-                    <path d="M5 12h14"></path>
-                    <path d="M12 5l7 7-7 7"></path>
-                  </svg>
-                </a>
+                <Link to="">
+                  <a class="text-white inline-flex items-center mt-4">
+                    Learn More
+                    <svg
+                      class="w-4 h-4 ml-2"
+                      viewBox="0 0 24 24"
+                      stroke="#ffffff"
+                      stroke-width="2"
+                      fill="none"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    >
+                      <path d="M5 12h14"></path>
+                      <path d="M12 5l7 7-7 7"></path>
+                    </svg>
+                  </a>
+                </Link>
               </div>
             </div>
             <div class="py-8 flex flex-wrap md:flex-nowrap">
               <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                <span class="font-semibold title-font text-gray-700">
+                <span class="font-semibold title-font text-white">
                   CATEGORY
                 </span>
-                <span class="text-sm text-gray-500">12 Jun 2019</span>
+                <span class="text-sm text-white">12 Jun 2019</span>
               </div>
               <div class="md:flex-grow">
-                <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">
+                <h2 class="text-2xl font-medium text-white title-font mb-2">
                   Woke master cleanse drinking vinegar salvia
                 </h2>
                 <p class="leading-relaxed">
@@ -98,21 +103,23 @@ const RecentArticle = () => {
                   moon party messenger bag selfies, poke vaporware kombucha
                   lumbersexual pork belly polaroid hoodie portland craft beer.
                 </p>
-                <a class="text-indigo-500 inline-flex items-center mt-4">
-                  Learn More
-                  <svg
-                    class="w-4 h-4 ml-2"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    fill="none"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  >
-                    <path d="M5 12h14"></path>
-                    <path d="M12 5l7 7-7 7"></path>
-                  </svg>
-                </a>
+                <Link to="">
+                  <a class="text-white inline-flex items-center mt-4">
+                    Learn More
+                    <svg
+                      class="w-4 h-4 ml-2"
+                      viewBox="0 0 24 24"
+                      stroke="#ffffff"
+                      stroke-width="2"
+                      fill="none"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    >
+                      <path d="M5 12h14"></path>
+                      <path d="M12 5l7 7-7 7"></path>
+                    </svg>
+                  </a>
+                </Link>
               </div>
             </div>
           </div>
