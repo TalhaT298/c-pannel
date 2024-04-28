be4d7d8ec3a433d147a1864f68c3175af8667f99
Talha <mdtalha2008@gmail.com>
2024-04-28 19:53:05 +0600

    packman add

--- src/pages/Home/ThreeD/ThreeD.jsx
+++ src/pages/Home/ThreeD/ThreeD.jsx
@@ -1,28 +1,40 @@
-import React, { useEffect } from 'react';
-import TagCloud from 'TagCloud'; // Assuming TagCloud is a valid package or component
+// import React, { useEffect } from 'react';
+// import TagCloud from 'TagCloud'; // Assuming TagCloud is a valid package or component
+
+// const ThreeD = () => {
+//     useEffect(() => {
+//         // Your TagCloud initialization code
+//         const container = '.tagcloud';
+//         const texts = [
+//             '3D', 'TagCloud', 'JavaScript',
+//             'CSS3', 'Animation', 'Interactive',
+//             'Mouse', 'Rolling', 'Sphere',
+//             '6KB', 'v2.x',
+//         ];
+//         const options = {};
+        
+//         TagCloud(container, texts, options);
+//     }, []); // Empty dependency array ensures this effect runs only once after initial render
+
+//     return (
+       
+            
+//             <div className="tagcloud">
+//                 {/* TagCloud content will be generated here */}
+//             </div>
+           
+        
+//     );
+// };
+
+// export default ThreeD;
+import React from 'react';
+import "./Three.css"
 
 const ThreeD = () => {
-    useEffect(() => {
-        // Your TagCloud initialization code
-        const container = '.tagcloud';
-        const texts = [
-            '3D', 'TagCloud', 'JavaScript',
-            'CSS3', 'Animation', 'Interactive',
-            'Mouse', 'Rolling', 'Sphere',
-            '6KB', 'v2.x',
-        ];
-        const options = {};
-        
-        TagCloud(container, texts, options);
-    }, []); // Empty dependency array ensures this effect runs only once after initial render
-
     return (
-        <div className="flex justify-center">
-            
-            <div className="tagcloud">
-                {/* TagCloud content will be generated here */}
-            </div>
-           
+        <div class="ok">
+            <div class="pacman-loader" aria-live="polite">Loading...</div>
         </div>
     );
 };
