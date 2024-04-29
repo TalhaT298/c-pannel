99898f459d3f2777e49d99b4b1864bb5964c2fc0
Talha <mdtalha2008@gmail.com>
2024-04-29 17:12:27 +0600

    calendy add successfully

--- src/pages/Schedule/Schedule.jsx
+++ src/pages/Schedule/Schedule.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+// import { PopupButton } from "react-calendly";
 
 const Schedule = () => {
     return (
         <div>
             <Link to="/sched"></Link>
-            ok
+            {/* <PopupButton
+                url="https://calendly.com/tariquetalha6/oceancapp"
+                
+                rootElement={document.getElementById("root")}
+                text="Click here to schedule!"
+                textColor="#ffffff"
+                color="#00a2ff"
+            /> */}
+            <div>
+                <iframe 
+                
+                
+                src="https://calendly.com/tariquetalha6/oceancapp" 
+                width="100%"
+                height="600"
+                frameborder="0"></iframe>
+            </div>
         </div>
     );
 };
