9ff183cc554e774db6f1bc6866b62297a04d7f80
Talha <mdtalha2008@gmail.com>
2024-05-17 18:57:28 +0600

    recent article update

--- src/pages/Home/RecentArticle/RecentArticle.jsx
+++ src/pages/Home/RecentArticle/RecentArticle.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 import { Link } from "react-router-dom";
 
@@ -10,37 +11,40 @@ const RecentArticle = () => {
           recent articles
         </h3>
       </div>
-      <div class="h-1 w-80 bg-white rounded flex justify-center items-center mx-auto my-auto hover:animate-ping"></div>
-      <section class="text-white body-font overflow-hidden">
-        <div class="container px-5 py-24 mx-auto">
-          <div class="-my-8 divide-y-2 divide-gray-100">
-            <div class="py-8 flex flex-wrap md:flex-nowrap">
-              <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                <span class="font-semibold title-font text-white">
+      <div className="h-1 w-80 bg-white rounded flex justify-center items-center mx-auto my-auto hover:animate-ping"></div>
+      <section className="text-white body-font overflow-hidden">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="-my-8 divide-y-2 divide-gray-100">
+            <div className="py-8 flex flex-wrap md:flex-nowrap">
+              <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+                <span className="font-semibold title-font text-white">
                   CATEGORY
                 </span>
-                <span class="mt-1 text-white text-sm">12 Jun 2019</span>
+                <span className="mt-1 text-white text-sm">12 Jun 2019</span>
               </div>
-              <div class="md:flex-grow">
-                <h2 class="text-2xl font-medium text-white title-font mb-2">
+              <div className="md:flex-grow">
+                <h2 className="text-2xl font-medium text-white title-font mb-2">
                   Bitters hashtag waistcoat fashion axe chia unicorn
                 </h2>
-                <p class="leading-relaxed">
+                <p className="leading-relaxed">
                   Glossier echo park pug, church-key sartorial biodiesel
                   vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf
                   moon party messenger bag selfies, poke vaporware kombucha
                   lumbersexual pork belly polaroid hoodie portland craft beer.
                 </p>
                 <Link to="">
-                  <a class="text-white inline-flex items-center mt-4">
+                  <a className="text-white inline-flex items-center mt-4">
                     Learn More
                     <svg
-                      class="w-4 h-4 ml-2"
+                      className="w-4 h-4 ml-2"
                       viewBox="0 0 24 24"
                       stroke="#ffffff"
+                      // eslint-disable-next-line react/no-unknown-property
                       stroke-width="2"
                       fill="none"
+                      // eslint-disable-next-line react/no-unknown-property
                       stroke-linecap="round"
+                      // eslint-disable-next-line react/no-unknown-property
                       stroke-linejoin="round"
                     >
                       <path d="M5 12h14"></path>
@@ -50,33 +54,36 @@ const RecentArticle = () => {
                 </Link>
               </div>
             </div>
-            <div class="py-8 flex flex-wrap md:flex-nowrap">
-              <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                <span class="font-semibold title-font text-white">
+            <div className="py-8 flex flex-wrap md:flex-nowrap">
+              <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+                <span className="font-semibold title-font text-white">
                   CATEGORY
                 </span>
-                <span class="mt-1 text-white text-sm">12 Jun 2019</span>
+                <span className="mt-1 text-white text-sm">12 Jun 2019</span>
               </div>
-              <div class="md:flex-grow">
-                <h2 class="text-2xl font-medium text-white title-font mb-2">
+              <div className="md:flex-grow">
+                <h2 className="text-2xl font-medium text-white title-font mb-2">
                   Meditation bushwick direct trade taxidermy shaman
                 </h2>
-                <p class="leading-relaxed">
+                <p className="leading-relaxed">
                   Glossier echo park pug, church-key sartorial biodiesel
                   vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf
                   moon party messenger bag selfies, poke vaporware kombucha
                   lumbersexual pork belly polaroid hoodie portland craft beer.
                 </p>
                 <Link to="">
-                  <a class="text-white inline-flex items-center mt-4">
+                  <a className="text-white inline-flex items-center mt-4">
                     Learn More
                     <svg
-                      class="w-4 h-4 ml-2"
+                     className="w-4 h-4 ml-2"
                       viewBox="0 0 24 24"
                       stroke="#ffffff"
+                      // eslint-disable-next-line react/no-unknown-property
                       stroke-width="2"
                       fill="none"
+                      // eslint-disable-next-line react/no-unknown-property
                       stroke-linecap="round"
+                      // eslint-disable-next-line react/no-unknown-property
                       stroke-linejoin="round"
                     >
                       <path d="M5 12h14"></path>
@@ -86,33 +93,36 @@ const RecentArticle = () => {
                 </Link>
               </div>
             </div>
-            <div class="py-8 flex flex-wrap md:flex-nowrap">
-              <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                <span class="font-semibold title-font text-white">
+            <div className="py-8 flex flex-wrap md:flex-nowrap">
+              <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+                <span className="font-semibold title-font text-white">
                   CATEGORY
                 </span>
-                <span class="text-sm text-white">12 Jun 2019</span>
+                <span className="text-sm text-white">12 Jun 2019</span>
               </div>
-              <div class="md:flex-grow">
-                <h2 class="text-2xl font-medium text-white title-font mb-2">
+              <div className="md:flex-grow">
+                <h2 className="text-2xl font-medium text-white title-font mb-2">
                   Woke master cleanse drinking vinegar salvia
                 </h2>
-                <p class="leading-relaxed">
+                <p className="leading-relaxed">
                   Glossier echo park pug, church-key sartorial biodiesel
                   vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf
                   moon party messenger bag selfies, poke vaporware kombucha
                   lumbersexual pork belly polaroid hoodie portland craft beer.
                 </p>
                 <Link to="">
-                  <a class="text-white inline-flex items-center mt-4">
+                  <a className="text-white inline-flex items-center mt-4">
                     Learn More
                     <svg
-                      class="w-4 h-4 ml-2"
+                      className="w-4 h-4 ml-2"
                       viewBox="0 0 24 24"
                       stroke="#ffffff"
+                      // eslint-disable-next-line react/no-unknown-property
                       stroke-width="2"
                       fill="none"
+                      // eslint-disable-next-line react/no-unknown-property
                       stroke-linecap="round"
+                      // eslint-disable-next-line react/no-unknown-property
                       stroke-linejoin="round"
                     >
                       <path d="M5 12h14"></path>
