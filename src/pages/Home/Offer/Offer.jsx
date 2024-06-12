fac39e356060c78f9baabe193eeda535ff2826ea
Talha <mdtalha2008@gmail.com>
2024-06-13 02:30:33 +0600

    offer update

--- src/pages/Home/Offer/Offer.jsx
+++ src/pages/Home/Offer/Offer.jsx
@@ -9,7 +9,7 @@ import img1 from "../../../assets/Web_design (2).webp";
 import img2 from "../../../assets/Web_design (3).webp";
 import img3 from "../../../assets/Web_design (1).webp";
 import img4 from "../../../assets/Web_design (5).webp";
-import "./Offer.css"; 
+import "./Offer.css";
 const Offer = () => {
   return (
     <div>
@@ -21,53 +21,46 @@ const Offer = () => {
       </div>
       <div className="h-1 w-80 bg-cyan-500 rounded flex justify-center items-center mx-auto my-auto hover:animate-ping"></div>
       <div className="flex flex-col md:flex-row md:flex-wrap justify-center items-center my-10">
-       
-      <div
-  className="card bg-base-100 mx-4 mb-8 md:w-96 rounded custom-gradient"
-  style={{
-    boxShadow:
-      "0 4px 6px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.08), 0 0 0 1px rgba(255, 255, 255, 0.5)",
-  }}
->
-  <figure className="px-4 pt-10">
-    <img src={img2} alt="Shoes" className="rounded-xl" />
-  </figure>
-  <div className="card-body text-center flex justify-center items-center">
-    <h2 className="card-title">WordPress development</h2>
-    <p>
-      Our skilled WordPress developers create custom, user-friendly, and
-      SEO-optimized websites that are easy to manage and tailored to your
-      needs.
-    </p>
-    <div className="card-actions card-actions flex justify-center items-center">
-      <Link to="/gameDevelopment">
-        <button className="btn btn-outline w-full bg-black text-white border-black border group hover:bg-white hover:text-black">
-          Learn More
-          <PiArrowRightLight
-            size="1.1rem"
-            className="text-white group-hover:text-black"
-          />
-        </button>
-      </Link>
-    </div>
-  </div>
-</div>
-
-
         <div
-          
-          className="card bg-base-100  mx-4 mb-8 md:w-96 rounded "
+          className="card bg-base-100 mx-4 mb-8 md:w-96 rounded custom-gradient"
           style={{
             boxShadow:
               "0 4px 6px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.08), 0 0 0 1px rgba(255, 255, 255, 0.5)",
           }}
         >
           <figure className="px-4 pt-10">
-            <img
-              src={img3}
-              alt="Shoes"
-              className="rounded-xl"
-            />
+            <img src={img2} alt="Shoes" className="rounded-xl" />
+          </figure>
+          <div className="card-body text-center flex justify-center items-center">
+            <h2 className="card-title">WordPress development</h2>
+            <p>
+              Our skilled WordPress developers create custom, user-friendly, and
+              SEO-optimized websites that are easy to manage and tailored to
+              your needs.
+            </p>
+            <div className="card-actions card-actions flex justify-center items-center">
+              <Link to="/gameDevelopment">
+                <button className="btn btn-outline w-full bg-black text-white border-black border group hover:bg-white hover:text-black">
+                  Learn More
+                  <PiArrowRightLight
+                    size="1.1rem"
+                    className="text-white group-hover:text-black"
+                  />
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        <div
+          className="card bg-base-100 mx-4 mb-8 md:w-96 rounded custom-gradient"
+          style={{
+            boxShadow:
+              "0 4px 6px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.08), 0 0 0 1px rgba(255, 255, 255, 0.5)",
+          }}
+        >
+          <figure className="px-4 pt-10">
+            <img src={img3} alt="Shoes" className="rounded-xl" />
           </figure>
           <div className="card-body text-center flex justify-center items-center">
             <h2 className="card-title">Web Development</h2>
@@ -78,10 +71,7 @@ const Offer = () => {
             </p>
             <div className="card-actions card-actions flex justify-center items-center">
               <Link to="/webdevelopment">
-                <button
-                  
-                  className="btn btn-outline w-full bg-black text-white border-black border group hover:bg-white hover:text-black"
-                >
+                <button className="btn btn-outline w-full bg-black text-white border-black border group hover:bg-white hover:text-black">
                   Learn More
                   <PiArrowRightLight
                     size="1.1rem"
@@ -96,18 +86,14 @@ const Offer = () => {
         <div
           // whileHover={{ scale: 1.05 }}
           // whileTap={{ scale: 0.8 }}
-          className="card bg-base-100  mx-4 mb-8 md:w-96 rounded "
+          className="card bg-base-100 mx-4 mb-8 md:w-96 rounded custom-gradient"
           style={{
             boxShadow:
               "0 4px 6px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.08), 0 0 0 1px rgba(255, 255, 255, 0.5)",
           }}
         >
           <figure className="px-4 pt-10">
-            <img
-              src={img4}
-              alt="Shoes"
-              className="rounded-xl"
-            />
+            <img src={img4} alt="Shoes" className="rounded-xl" />
           </figure>
           <div className="card-body text-center flex justify-center items-center">
             <h2 className="card-title">Cyber Security</h2>
@@ -118,10 +104,7 @@ const Offer = () => {
             </p>
             <div className="card-actions card-actions flex justify-center items-center">
               <Link to="/cybersecurity">
-                <button
-                  
-                  className="btn btn-outline w-full bg-black text-white border-black border group hover:bg-white hover:text-black"
-                >
+                <button className="btn btn-outline w-full bg-black text-white border-black border group hover:bg-white hover:text-black">
                   Learn More
                   <PiArrowRightLight
                     size="1.1rem"
@@ -133,23 +116,18 @@ const Offer = () => {
           </div>
         </div>
 
-
-          {/* 2nd section */}
-          <div
+        {/* 2nd section */}
+        <div
           // whileHover={{ scale: 1.05 }}
           // whileTap={{ scale: 0.8 }}
-          className="card bg-base-100  mx-4 mb-8 md:w-96 rounded "
+          className="card bg-base-100 mx-4 mb-8 md:w-96 rounded custom-gradient"
           style={{
             boxShadow:
               "0 4px 6px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.08), 0 0 0 1px rgba(255, 255, 255, 0.5)",
           }}
         >
           <figure className="px-4 pt-10">
-            <img
-              src={img1}
-              alt="Shoes"
-              className="rounded-xl"
-            />
+            <img src={img1} alt="Shoes" className="rounded-xl" />
           </figure>
           <div className="card-body text-center flex justify-center items-center">
             <h2 className="card-title">Web Design</h2>
@@ -159,10 +137,7 @@ const Offer = () => {
             </p>
             <div className="card-actions card-actions flex justify-center items-center">
               <Link to="/webdesign">
-                <button
-                  
-                  className="btn btn-outline w-full bg-black text-white border-black border group hover:bg-white hover:text-black"
-                >
+                <button className="btn btn-outline w-full bg-black text-white border-black border group hover:bg-white hover:text-black">
                   Learn More
                   <PiArrowRightLight
                     size="1.1rem"
@@ -175,9 +150,9 @@ const Offer = () => {
         </div>
 
         <div
-          whileHover={{ scale: 1.05 }}
-          whileTap={{ scale: 0.8 }}
-          className="card bg-base-100  mx-4 mb-8 md:w-96 rounded "
+          // whileHover={{ scale: 1.05 }}
+          // whileTap={{ scale: 0.8 }}
+          className="card bg-base-100 mx-4 mb-8 md:w-96 rounded custom-gradient"
           style={{
             boxShadow:
               "0 4px 6px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.08), 0 0 0 1px rgba(255, 255, 255, 0.5)",
@@ -193,14 +168,14 @@ const Offer = () => {
           <div className="card-body text-center flex justify-center items-center">
             <h2 className="card-title">Prompt Engineering</h2>
             <p>
-            Our proficient prompt engineers specialize in designing and optimizing AI prompts to enhance user interactions, ensuring precise, efficient, and meaningful responses tailored to your business needs
+              Our proficient prompt engineers specialize in designing and
+              optimizing AI prompts to enhance user interactions, ensuring
+              precise, efficient, and meaningful responses tailored to your
+              business needs
             </p>
             <div className="card-actions card-actions flex justify-center items-center">
               <Link to="/cricket">
-                <button
-                
-                  className="btn btn-outline w-full bg-black text-white border-black border group hover:bg-white hover:text-black"
-                >
+                <button className="btn btn-outline w-full bg-black text-white border-black border group hover:bg-white hover:text-black">
                   Learn More
                   <PiArrowRightLight
                     size="1.1rem"
@@ -257,8 +232,7 @@ const Offer = () => {
           </div>
         </motion.div> */}
         <div
-         
-          className="card bg-base-100  mx-4 mb-8 md:w-96 rounded "
+          className="card bg-base-100 mx-4 mb-8 md:w-96 rounded custom-gradient"
           style={{
             boxShadow:
               "0 4px 6px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.08), 0 0 0 1px rgba(255, 255, 255, 0.5)",
@@ -274,14 +248,14 @@ const Offer = () => {
           <div className="card-body text-center flex justify-center items-center">
             <h2 className="card-title">Graphics Design</h2>
             <p>
-            Our talented graphic designers craft visually compelling and innovative designs that effectively communicate your brand message, ensuring your visuals stand out and captivate your audience.
+              Our talented graphic designers craft visually compelling and
+              innovative designs that effectively communicate your brand
+              message, ensuring your visuals stand out and captivate your
+              audience.
             </p>
             <div className="card-actions card-actions flex justify-center items-center">
               <Link to="/cricket">
-                <button
-                  
-                  className="btn btn-outline w-full bg-black text-white border-black border group hover:bg-white hover:text-black"
-                >
+                <button className="btn btn-outline w-full bg-black text-white border-black border group hover:bg-white hover:text-black">
                   Learn More
                   <PiArrowRightLight
                     size="1.1rem"
