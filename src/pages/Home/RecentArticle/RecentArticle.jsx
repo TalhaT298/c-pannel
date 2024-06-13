3f8d41cda8897e9bf67ba43158fdbee12c7abad8
Talha <mdtalha2008@gmail.com>
2024-06-13 16:50:10 +0600

    post about it

--- src/pages/Home/RecentArticle/RecentArticle.jsx
+++ src/pages/Home/RecentArticle/RecentArticle.jsx
@@ -125,7 +125,7 @@ const RecentArticle = () => {
                     height="22"
                     viewBox="0 0 21 22"
                   >
-                    <FaShareAlt  className="" style={{ fontSize: '26px' }} />
+                    <FaShareAlt  className="" style={{ fontSize: '20px' }} />
                   </svg>
                   <h2 className="text-lg font-semibold text-slate-800 dark:text-white/90">
                     34
@@ -138,8 +138,339 @@ const RecentArticle = () => {
               </div>
             </div>
           </SwiperSlide>
-          <SwiperSlide>Slide 2</SwiperSlide>
-          <SwiperSlide>Slide 3</SwiperSlide>
+          {/* pat2 ---------------------------*/}
+          <SwiperSlide>
+            {" "}
+            <div className=" mx-auto my-6 max-w-[350px] rounded-lg bg-white font-sans shadow-lg dark:bg-[#18181B]">
+              <div className="flex items-center justify-between gap-10 px-4 py-4">
+                {/* Avatar image  */}
+                <div className="flex items-center gap-3">
+                  <img
+                    width={100}
+                    height={100}
+                    className="h-16 w-16 rounded-full bg-black/40"
+                    src="https://source.unsplash.com/100x100/?portrait"
+                    alt="card navigate ui"
+                  />
+                  <div className="flex flex-col">
+                    <h2 className="text-xl font-semibold">Yoan Lee</h2>
+                    <p className="text-gray-400">2 days ago</p>
+                  </div>
+                </div>
+                {/* Setting button */}
+                <div className="flex cursor-pointer flex-col gap-2 rounded-full px-4 py-3">
+                  <span className="size-[3px] rounded-full bg-gray-600"></span>
+                  <span className="size-[3px] rounded-full bg-gray-600"></span>
+                  <span className="size-[3px] rounded-full bg-gray-600"></span>
+                </div>
+              </div>
+              {/* Post Image */}
+              <div className="flex flex-col gap-1">
+                <div className="w-full">
+                  <img
+                    width={400}
+                    height={200}
+                    className="h-[150px] w-full bg-black/40"
+                    src="https://source.unsplash.com/400x200/?arts"
+                    alt="card navigate ui"
+                  />
+                </div>
+                <div className="flex gap-1 overflow-hidden">
+                  <img
+                    width={201}
+                    height={201}
+                    className="h-[150px] w-[49.5%] bg-black/40"
+                    src="https://source.unsplash.com/201x201/?arts"
+                    alt="card navigate ui"
+                  />
+                  <img
+                    width={202}
+                    height={202}
+                    className="h-[150px] w-[49.5%] bg-black/40"
+                    src="https://source.unsplash.com/202x202/?arts"
+                    alt="card navigate ui"
+                  />
+                </div>
+              </div>
+              {/* Post content */}
+              <div className="mt-3 space-y-2 px-4">
+                <h2 className="text-xl font-semibold text-slate-800 dark:text-white/90">
+                  My new paintings
+                </h2>
+                <h2 className="text-sm text-gray-500 dark:text-white/50">
+                  Hey, I captured some picture recently. You can check it and
+                  share you thoughts with me. I want to listen your thoughts...{" "}
+                  <span className="cursor-pointer text-[#3e96d4]">
+                    See more
+                  </span>
+                </h2>
+              </div>
+              {/* icons */}
+              <div className="mt-4 flex justify-between px-4 pb-4">
+                <div className="flex items-center gap-2">
+                  <svg
+                    className="w-6 fill-[#1E293B] dark:fill-white/90"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <g strokeWidth="0"></g>
+                    <g
+                      id="navigateui"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    ></g>
+                    <g id="navigateui">
+                    <GiSelfLove className="" style={{ fontSize: '26px' }}/>
+                    </g>
+                  </svg>
+                  <h2 className="text-lg font-semibold text-slate-800 dark:text-white/90">
+                    10k
+                  </h2>
+                </div>
+                <div className="flex items-center gap-2">
+                  <svg
+                    className="fill-[#1E293B] dark:fill-white/90"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="21"
+                    height="22"
+                    viewBox="0 0 21 22"
+                  >
+                    <FaShareAlt  className="" style={{ fontSize: '20px' }} />
+                  </svg>
+                  <h2 className="text-lg font-semibold text-slate-800 dark:text-white/90">
+                    34
+                  </h2>
+                </div>
+                <div className="flex items-center gap-2">
+                  
+                  
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+          {/* pat2 end------------------------------------------------------------------------*/}
+          {/* part3 ----------------------------*/}
+          <SwiperSlide>
+            {" "}
+            <div className=" mx-auto my-6 max-w-[350px] rounded-lg bg-white font-sans shadow-lg dark:bg-[#18181B]">
+              <div className="flex items-center justify-between gap-10 px-4 py-4">
+                {/* Avatar image  */}
+                <div className="flex items-center gap-3">
+                  <img
+                    width={100}
+                    height={100}
+                    className="h-16 w-16 rounded-full bg-black/40"
+                    src="https://source.unsplash.com/100x100/?portrait"
+                    alt="card navigate ui"
+                  />
+                  <div className="flex flex-col">
+                    <h2 className="text-xl font-semibold">Yoan Lee</h2>
+                    <p className="text-gray-400">2 days ago</p>
+                  </div>
+                </div>
+                {/* Setting button */}
+                <div className="flex cursor-pointer flex-col gap-2 rounded-full px-4 py-3">
+                  <span className="size-[3px] rounded-full bg-gray-600"></span>
+                  <span className="size-[3px] rounded-full bg-gray-600"></span>
+                  <span className="size-[3px] rounded-full bg-gray-600"></span>
+                </div>
+              </div>
+              {/* Post Image */}
+              <div className="flex flex-col gap-1">
+                <div className="w-full">
+                  <img
+                    width={400}
+                    height={200}
+                    className="h-[150px] w-full bg-black/40"
+                    src="https://source.unsplash.com/400x200/?arts"
+                    alt="card navigate ui"
+                  />
+                </div>
+                <div className="flex gap-1 overflow-hidden">
+                  <img
+                    width={201}
+                    height={201}
+                    className="h-[150px] w-[49.5%] bg-black/40"
+                    src="https://source.unsplash.com/201x201/?arts"
+                    alt="card navigate ui"
+                  />
+                  <img
+                    width={202}
+                    height={202}
+                    className="h-[150px] w-[49.5%] bg-black/40"
+                    src="https://source.unsplash.com/202x202/?arts"
+                    alt="card navigate ui"
+                  />
+                </div>
+              </div>
+              {/* Post content */}
+              <div className="mt-3 space-y-2 px-4">
+                <h2 className="text-xl font-semibold text-slate-800 dark:text-white/90">
+                  My new paintings
+                </h2>
+                <h2 className="text-sm text-gray-500 dark:text-white/50">
+                  Hey, I captured some picture recently. You can check it and
+                  share you thoughts with me. I want to listen your thoughts...{" "}
+                  <span className="cursor-pointer text-[#3e96d4]">
+                    See more
+                  </span>
+                </h2>
+              </div>
+              {/* icons */}
+              <div className="mt-4 flex justify-between px-4 pb-4">
+                <div className="flex items-center gap-2">
+                  <svg
+                    className="w-6 fill-[#1E293B] dark:fill-white/90"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <g strokeWidth="0"></g>
+                    <g
+                      id="navigateui"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    ></g>
+                    <g id="navigateui">
+                    <GiSelfLove className="" style={{ fontSize: '26px' }}/>
+                    </g>
+                  </svg>
+                  <h2 className="text-lg font-semibold text-slate-800 dark:text-white/90">
+                    10k
+                  </h2>
+                </div>
+                <div className="flex items-center gap-2">
+                  <svg
+                    className="fill-[#1E293B] dark:fill-white/90"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="21"
+                    height="22"
+                    viewBox="0 0 21 22"
+                  >
+                    <FaShareAlt  className="" style={{ fontSize: '20px' }} />
+                  </svg>
+                  <h2 className="text-lg font-semibold text-slate-800 dark:text-white/90">
+                    34
+                  </h2>
+                </div>
+                <div className="flex items-center gap-2">
+                  
+                  
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+          {/* parth3 end--------------------------------------------- */}
+          {/* part4 ------------*/}
+          <SwiperSlide>
+            {" "}
+            <div className=" mx-auto my-6 max-w-[350px] rounded-lg bg-white font-sans shadow-lg dark:bg-[#18181B]">
+              <div className="flex items-center justify-between gap-10 px-4 py-4">
+                {/* Avatar image  */}
+                <div className="flex items-center gap-3">
+                  <img
+                    width={100}
+                    height={100}
+                    className="h-16 w-16 rounded-full bg-black/40"
+                    src="https://source.unsplash.com/100x100/?portrait"
+                    alt="card navigate ui"
+                  />
+                  <div className="flex flex-col">
+                    <h2 className="text-xl font-semibold">Yoan Lee</h2>
+                    <p className="text-gray-400">2 days ago</p>
+                  </div>
+                </div>
+                {/* Setting button */}
+                <div className="flex cursor-pointer flex-col gap-2 rounded-full px-4 py-3">
+                  <span className="size-[3px] rounded-full bg-gray-600"></span>
+                  <span className="size-[3px] rounded-full bg-gray-600"></span>
+                  <span className="size-[3px] rounded-full bg-gray-600"></span>
+                </div>
+              </div>
+              {/* Post Image */}
+              <div className="flex flex-col gap-1">
+                <div className="w-full">
+                  <img
+                    width={400}
+                    height={200}
+                    className="h-[150px] w-full bg-black/40"
+                    src="https://source.unsplash.com/400x200/?arts"
+                    alt="card navigate ui"
+                  />
+                </div>
+                <div className="flex gap-1 overflow-hidden">
+                  <img
+                    width={201}
+                    height={201}
+                    className="h-[150px] w-[49.5%] bg-black/40"
+                    src="https://source.unsplash.com/201x201/?arts"
+                    alt="card navigate ui"
+                  />
+                  <img
+                    width={202}
+                    height={202}
+                    className="h-[150px] w-[49.5%] bg-black/40"
+                    src="https://source.unsplash.com/202x202/?arts"
+                    alt="card navigate ui"
+                  />
+                </div>
+              </div>
+              {/* Post content */}
+              <div className="mt-3 space-y-2 px-4">
+                <h2 className="text-xl font-semibold text-slate-800 dark:text-white/90">
+                  My new paintings
+                </h2>
+                <h2 className="text-sm text-gray-500 dark:text-white/50">
+                  Hey, I captured some picture recently. You can check it and
+                  share you thoughts with me. I want to listen your thoughts...{" "}
+                  <span className="cursor-pointer text-[#3e96d4]">
+                    See more
+                  </span>
+                </h2>
+              </div>
+              {/* icons */}
+              <div className="mt-4 flex justify-between px-4 pb-4">
+                <div className="flex items-center gap-2">
+                  <svg
+                    className="w-6 fill-[#1E293B] dark:fill-white/90"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <g strokeWidth="0"></g>
+                    <g
+                      id="navigateui"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    ></g>
+                    <g id="navigateui">
+                    <GiSelfLove className="" style={{ fontSize: '26px' }}/>
+                    </g>
+                  </svg>
+                  <h2 className="text-lg font-semibold text-slate-800 dark:text-white/90">
+                    10k
+                  </h2>
+                </div>
+                <div className="flex items-center gap-2">
+                  <svg
+                    className="fill-[#1E293B] dark:fill-white/90"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="21"
+                    height="22"
+                    viewBox="0 0 21 22"
+                  >
+                    <FaShareAlt  className="" style={{ fontSize: '20px' }} />
+                  </svg>
+                  <h2 className="text-lg font-semibold text-slate-800 dark:text-white/90">
+                    34
+                  </h2>
+                </div>
+                <div className="flex items-center gap-2">
+                  
+                  
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+          {/* part4end ---------------------------------------------------------------------------------------*/}
           <SwiperSlide>Slide 4</SwiperSlide>
           <SwiperSlide>Slide 5</SwiperSlide>
           <SwiperSlide>Slide 6</SwiperSlide>
