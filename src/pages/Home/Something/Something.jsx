b29917e72d449104e0d0190b74851109ecc62c4f
Talha <mdtalha2008@gmail.com>
2024-06-18 21:00:51 +0600

    title add at something

--- src/pages/Home/Something/Something.jsx
+++ src/pages/Home/Something/Something.jsx
@@ -5,7 +5,7 @@ const Something = () => {
   return (
     <div>
       <div className="flex">
-        <div className="w-2/5">Left Side</div>
+        <div className="w-2/5 text-3xl font-bold">Oceancapp: Navigating Global Projects with Expertise and Assurance</div>
         <div className="w-3/5">Right Side</div>
       </div>
     </div>
