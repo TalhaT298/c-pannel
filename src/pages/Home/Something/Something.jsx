8167fdf29ccaec4f26ffd1aab0bfbab00089260e
Talha <mdtalha2008@gmail.com>
2024-06-19 02:03:38 +0600

    data aos install perfect

--- src/pages/Home/Something/Something.jsx
+++ src/pages/Home/Something/Something.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect } from "react";
 import AOS from 'aos';
-
+import 'aos/dist/aos.css';
 const Something = () => {
   useEffect(() => {
     AOS.init({
