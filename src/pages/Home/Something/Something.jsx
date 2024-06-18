bd22f168657a5e11f5c5fc393f7b959344352a47
Talha <mdtalha2008@gmail.com>
2024-06-18 23:03:07 +0600

    text perfect

--- src/pages/Home/Something/Something.jsx
+++ src/pages/Home/Something/Something.jsx
@@ -5,7 +5,7 @@ const Something = () => {
   return (
     <div>
       <div className="flex gap-8">
-        <div className="w-1/2 text-5xl font-bold leading-relaxed">Oceancapp: Navigating Global Projects with Expertise and Assurance</div>
+        <div className="w-1/2 text-5xl font-bold leading-relaxed"> Navigating Global Projects with Expertise and Assurance</div>
         <div className="w-1/2 font-semibold font-thin pt-5">Discover Oceancapp, your trusted partner in managing global projects with precision and care. With over 147 experts, 359 client protections, and a 100% service guarantee, we ensure your projects sail smoothly and successfully. Join the 527+ satisfied clients who rely on Oceancapp for unparalleled expertise and peace of mind</div>
       </div>
     </div>
