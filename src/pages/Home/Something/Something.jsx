c779c3f8ad40ea36d6d1f5250a2ca523c8cd7082
Talha <mdtalha2008@gmail.com>
2024-06-19 00:53:28 +0600

    aos install

--- src/pages/Home/Something/Something.jsx
+++ src/pages/Home/Something/Something.jsx
@@ -5,8 +5,8 @@ const Something = () => {
   return (
     <div>
       <div className="flex gap-8">
-        <div className="w-1/2 text-5xl font-bold leading-relaxed"> Navigating Global Projects with Expertise and Assurance</div>
-        <div className="w-1/2 font-semibold font-thin pt-5">Discover Oceancapp, your trusted partner in managing global projects with precision and care. With over 147 experts, 359 client protections, and a 100% service guarantee, we ensure your projects sail smoothly and successfully. Join the 527+ satisfied clients who rely on Oceancapp for unparalleled expertise and peace of mind</div>
+        <div className="w-1/2 text-5xl font-bold leading-relaxed" data-aos="fade-right"> Navigating Global Projects with<span>Expertise </span>and <span>Assurance</span></div>
+        <div className="w-1/2 font-semibold font-thin pt-5" data-aos="fade-left">Discover Oceancapp, your trusted partner in managing global projects with precision and care. With over 147 experts, 359 client protections, and a 100% service guarantee, we ensure your projects sail smoothly and successfully. Join the 527+ satisfied clients who rely on Oceancapp for unparalleled expertise and peace of mind</div>
       </div>
     </div>
   );
