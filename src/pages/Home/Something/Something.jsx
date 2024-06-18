ee2ec60e1e8fc0647dc8aa91da4922a8f182e0d5
Talha <mdtalha2008@gmail.com>
2024-06-18 20:53:19 +0600

    add div

--- src/pages/Home/Something/Something.jsx
+++ src/pages/Home/Something/Something.jsx
@@ -1,12 +1,15 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React from "react";
 
 const Something = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <div className="flex">
+        <div className="w-2/5">Left Side</div>
+        <div className="w-3/5">Right Side</div>
+      </div>
+    </div>
+  );
 };
 
 export default Something;
