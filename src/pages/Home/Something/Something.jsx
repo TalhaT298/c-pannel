1e2c2d5d26096d5b844ec20d93f5e21b65f4875c
Talha <mdtalha2008@gmail.com>
2024-06-18 21:03:59 +0600

    paragraph add

--- src/pages/Home/Something/Something.jsx
+++ src/pages/Home/Something/Something.jsx
@@ -6,7 +6,7 @@ const Something = () => {
     <div>
       <div className="flex">
         <div className="w-2/5 text-3xl font-bold">Oceancapp: Navigating Global Projects with Expertise and Assurance</div>
-        <div className="w-3/5">Right Side</div>
+        <div className="w-3/5">Discover Oceancapp, your trusted partner in managing global projects with precision and care. With over 147 experts, 359 client protections, and a 100% service guarantee, we ensure your projects sail smoothly and successfully. Join the 527+ satisfied clients who rely on Oceancapp for unparalleled expertise and peace of mind</div>
       </div>
     </div>
   );
