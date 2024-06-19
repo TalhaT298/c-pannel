4644d7351bd87ec820c1be83ca2468710b370428
Talha <mdtalha2008@gmail.com>
2024-06-20 01:00:34 +0600

    span text update

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-unknown-property */
+/* eslint-disable no-unused-vars */
 import React from "react";
 import { FaFacebookF } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
@@ -19,21 +21,21 @@ const Footer = () => {
   return (
     <div>
       <footer className="text-white body-font" style={{backgroundImage: `url(${background})`, backgroundSize: "cover", backgroundPosition: "center", minHeight: "200px"}}>
-        <div class="container px-5 py-12 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
-          <div class="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left md:mt-0 mt-10">
-            <a class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+        <div className="container px-5 py-12 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
+          <div className="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left md:mt-0 mt-10">
+            <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
               <img src={logo} className="h-12" alt="" />
             </a>
-            <p class="mt-2 text-sm text-white">
+            <p className="mt-2 text-sm text-white">
               Air plant banjo lyft occupy retro adaptogen indego
             </p>
           </div>
-          <div class="flex-grow flex flex-wrap md:pr-20 -mb-10 md:text-left text-center order-first">
-            <div class="lg:w-1/4 md:w-1/2 w-full px-4">
-              <h2 class="title-font font-bold text-white tracking-widest text-base mb-3">
+          <div className="flex-grow flex flex-wrap md:pr-20 -mb-10 md:text-left text-center order-first">
+            <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+              <h2 className="title-font font-bold text-white tracking-widest text-base mb-3">
                 Company
               </h2>
-              <nav class="list-none mb-10">
+              <nav className="list-none mb-10">
                 <li className="pb-4">
                   <Link to="">
                     <a className="text-white text-sm">About Us</a>
@@ -42,24 +44,24 @@ const Footer = () => {
                 <li className="pb-4">
                   <Link to="">
                     {" "}
-                    <a class="text-white text-sm">Blog</a>
+                    <a className="text-white text-sm">Blog</a>
                   </Link>
                 </li>
                 <li className="pb-4">
                   <Link to="">
                     {" "}
-                    <a class="text-white text-sm">OceanCapp2.0</a>
+                    <a className="text-white text-sm">OceanCapp2.0</a>
                   </Link>
                 </li>
                 <li className="pb-4">
                   <Link to="">
-                    <a class="text-white text-sm">Product</a>
+                    <a className="text-white text-sm">Product</a>
                   </Link>
                 </li>
                 <li className="pb-4">
                   <Link to="">
                     {" "}
-                    <a class="text-white text-sm">Privacy Policy</a>
+                    <a className="text-white text-sm">Privacy Policy</a>
                   </Link>
                 </li>
                 {/* <li>
@@ -67,31 +69,31 @@ const Footer = () => {
                 </li> */}
               </nav>
             </div>
-            <div class="lg:w-1/4 md:w-1/2 w-full px-4">
-              <h2 class="title-font font-bold text-white tracking-widest text-base mb-3">
+            <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+              <h2 className="title-font font-bold text-white tracking-widest text-base mb-3">
                 Service
               </h2>
-              <nav class="list-none mb-10">
+              <nav className="list-none mb-10">
                 <li className="pb-4">
                   <Link to="">
-                    <a class="text-white text-sm">Team Augmentation</a>
+                    <a className="text-white text-sm">Team Augmentation</a>
                   </Link>
                 </li>
                 <li className="pb-4">
                   <Link to="">
                     {" "}
-                    <a class="text-white text-sm">Offshore Development</a>
+                    <a className="text-white text-sm">Offshore Development</a>
                   </Link>
                 </li>
                 <li className="pb-4">
                   <Link to="">
                     {" "}
-                    <a class="text-white text-sm">MVP Service</a>
+                    <a className="text-white text-sm">MVP Service</a>
                   </Link>
                 </li>
                 <li className="pb-4">
                   <Link to="">
-                    <a class="text-white text-sm">End to End Development</a>
+                    <a className="text-white text-sm">End to End Development</a>
                   </Link>
                 </li>
                 {/* <li>
@@ -105,52 +107,52 @@ const Footer = () => {
                 </li> */}
               </nav>
             </div>
-            <div class="lg:w-1/4 md:w-1/2 w-full px-4">
-              <h2 class="title-font font-bold text-white tracking-widest text-base mb-3">
+            <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+              <h2 className="title-font font-bold text-white tracking-widest text-base mb-3">
                 Hiring Developers
               </h2>
-              <nav class="list-none mb-10">
+              <nav className="list-none mb-10">
                 <li className="pb-4">
                   <Link to="">
-                    <a class="text-white text-sm">Hire Developers</a>
+                    <a className="text-white text-sm">Hire Developers</a>
                   </Link>
                 </li>
                 <li className="pb-4">
                   <Link to="">
                     {" "}
-                    <a class="text-white text-sm">Hire JavaScript Developers</a>
+                    <a className="text-white text-sm">Hire JavaScript Developers</a>
                   </Link>
                 </li>
                 <li className="pb-4">
                   <Link to="">
                     {" "}
-                    <a class="text-white text-sm">Hire Python Developers</a>
+                    <a className="text-white text-sm">Hire Python Developers</a>
                   </Link>
                 </li>
                 <li className="pb-4">
                   <Link to="">
-                    <a class="text-white text-sm">Hire Java Developers</a>
-                  </Link>
-                </li>
-                <li className="pb-4">
-                  <Link to="">
-                    {" "}
-                    <a class="text-white text-sm">Hire Golang Developers</a>
+                    <a className="text-white text-sm">Hire Java Developers</a>
                   </Link>
                 </li>
                 <li className="pb-4">
                   <Link to="">
                     {" "}
-                    <a class="text-white text-sm">Hire .NET Developers</a>
+                    <a className="text-white text-sm">Hire Golang Developers</a>
+                  </Link>
+                </li>
+                <li className="pb-4">
+                  <Link to="">
+                    {" "}
+                    <a className="text-white text-sm">Hire .NET Developers</a>
                   </Link>
                 </li>
               </nav>
             </div>
-            <div class="lg:w-1/4 md:w-1/2 w-full px-4">
-              <h2 class="title-font font-bold text-white tracking-widest text-base mb-3">
+            <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+              <h2 className="title-font font-bold text-white tracking-widest text-base mb-3">
                 Contact Us
               </h2>
-              <nav class="list-none mb-10">
+              <nav className="list-none mb-10">
                 <li className="pb-4">
                   <Link to="">
                   <div className="flex ">
@@ -193,15 +195,15 @@ const Footer = () => {
             </div>
           </div>
         </div>
-        <div class="h-[1px] w-full bg-white"></div>
-        <div class="">
-          <div class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
-            <p class="text-gray-500 text-sm text-center sm:text-left">
+        <div className="h-[1px] w-full bg-white"></div>
+        <div className="">
+          <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+            <p className="text-gray-500 text-sm text-center sm:text-left">
               © 2024 OceanCapp —
               <a
                 href="https://twitter.com/knyttneve"
                 rel="noopener noreferrer"
-                class="text-gray-600 ml-1"
+                className="text-gray-600 ml-1"
                 target="_blank"
                 previewlistener="true"
               >
@@ -209,7 +211,7 @@ const Footer = () => {
               </a>
             </p>
 
-            <div class="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
+            <div className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
               <div className="grid grid-flow-col gap-3">
                 <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
                   <Link to="">
