0138f161b2bf66b6bccb2ca5d3085db90f164476
Talha <mdtalha2008@gmail.com>
2024-06-26 02:51:46 +0600

    prompt add at link

--- src/pages/PromptEngineering/PromptEngineering.jsx
+++ src/pages/PromptEngineering/PromptEngineering.jsx
@@ -1,11 +1,15 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const PromptEngineering = () => {
     return (
+        <>
+        <Link to=""></Link>
         <div>
             this ius promt
         </div>
+        </>
     );
 };
 
