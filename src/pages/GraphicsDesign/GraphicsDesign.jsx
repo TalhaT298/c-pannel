bb82978c2fd0f161372acb41f55470aaedd2d31a
Talha <mdtalha2008@gmail.com>
2024-06-26 02:52:55 +0600

    link add at graphics

--- src/pages/GraphicsDesign/GraphicsDesign.jsx
+++ src/pages/GraphicsDesign/GraphicsDesign.jsx
@@ -1,11 +1,15 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const GraphicsDesign = () => {
     return (
+        <>
+        <Link to="/graphics"></Link>
         <div>
            this is graphics 
         </div>
+        </>
     );
 };
 
