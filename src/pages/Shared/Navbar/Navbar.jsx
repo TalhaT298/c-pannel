a1e5f893928763976957451a6488f7dbe65c734b
Talha <mdtalha2008@gmail.com>
2024-06-26 03:27:52 +0600

    update navbar

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -31,9 +31,9 @@ const Navbar = () => {
           <Link to="/"><img src={logo} className='w-48 h-16' alt="" /></Link>
         </div>
         <ul className="hidden items-center justify-between gap-10 md:flex">
-          <li className="group flex  cursor-pointer flex-col">
+          {/* <li className="group flex  cursor-pointer flex-col">
             Home<span className="mt-[2px] h-[3px] w-[0px] rounded-full bg-white transition-all duration-300 group-hover:w-full"></span>
-          </li>
+          </li> */}
           <Link to="/services">
           <li className="group flex  cursor-pointer flex-col">
             Services<span className="mt-[2px] h-[3px]  w-[0px] rounded-full bg-white transition-all duration-300 group-hover:w-full"></span>
@@ -57,9 +57,9 @@ const Navbar = () => {
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="cursor-pointer" > <line x1="4" x2="20" y1="12" y2="12" /> <line x1="4" x2="20" y1="6" y2="6" /><line x1="4" x2="20" y1="18" y2="18" /> </svg>
           {dropDownState && (
             <ul className=" z-10  gap-2  bg-[#000000]  absolute right-0 top-11 flex w-[200px] flex-col  rounded-lg   text-base ">
-              <li className="cursor-pointer  px-6 py-2 text-white rounded-t-lg hover:bg-bg-white ">
+              {/* <li className="cursor-pointer  px-6 py-2 text-white rounded-t-lg hover:bg-bg-white ">
                 Home
-              </li>
+              </li> */}
               <Link to="/services">
               <li className="cursor-pointer  px-6 py-2 text-white hover:bg-white ">
                 Services
