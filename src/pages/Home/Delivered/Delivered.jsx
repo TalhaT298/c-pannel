0b87e7b9f6d53e038091f103eb8121121bb8e094
Talha <mdtalha2008@gmail.com>
2024-06-26 22:00:17 +0600

    deliverd add

---
+++ src/pages/Home/Delivered/Delivered.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const Delivered = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Delivered;
