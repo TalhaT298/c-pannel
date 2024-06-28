e60145fd58704e176a4f4a807013c3b8dd899fbf
Talha <mdtalha2008@gmail.com>
2024-06-29 02:24:51 +0600

    chart add

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import { Link } from "react-router-dom";
-
+import './Contact.css';
 const Contact = () => {
   return (
     <div>
@@ -55,8 +55,10 @@ const Contact = () => {
         </div>
       </div>
       {/* prlin */}
-      <div>
-    
+      <div >
+      <div id="chart-container">
+        <canvas id="myChart"></canvas>
+    </div>
       </div>
     </div>
   );
