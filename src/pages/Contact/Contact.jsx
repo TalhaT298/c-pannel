c82c2e40dbf4ebb69a56560255759e7aa4b31ac2
Talha <mdtalha2008@gmail.com>
2024-06-29 01:42:16 +0600

    login update

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
       <div>
         <div className="hero bg-base-200 min-h-screen">
           <div className="hero-content flex-col lg:flex-row-reverse">
-            <div className="text-center lg:text-left">
+            <div className="text-center w-1/2 lg:text-left">
               <h1 className="text-5xl font-bold">Login now!</h1>
               <p className="py-6">
                 Provident cupiditate voluptatem et in. Quaerat fugiat ut
@@ -17,7 +17,7 @@ const Contact = () => {
                 repudiandae et a id nisi.
               </p>
             </div>
-            <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            <div className="card w-1/2 bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
               <form className="card-body">
                 <div className="form-control">
                   <label className="label">
