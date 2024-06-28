64190bbf8f6b40ddbdc622c82f822b11dd52ec51
Talha <mdtalha2008@gmail.com>
2024-06-29 01:33:42 +0600

    link add at contact

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,9 +1,14 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Contact = () => {
     return (
         <div>
-            
+            <Link to="/contact"></Link>
+            <div>
+                
+            </div>
         </div>
     );
 };
