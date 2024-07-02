9374bdeec2586936e23a6b0e1dcd12191ad51ce8
Talha <mdtalha2008@gmail.com>
2024-07-02 21:20:39 +0600

    link add at carrer

--- src/pages/Career/Career.jsx
+++ src/pages/Career/Career.jsx
@@ -1,11 +1,15 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Career = () => {
     return (
+        <>
+        <Link to="/career"></Link>
         <div>
-            
+            this is carrer
         </div>
+        </>
     );
 };
 
