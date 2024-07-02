854af7d4b67775242636e6c82aa5dfa5b48be7a4
Talha <mdtalha2008@gmail.com>
2024-07-02 22:22:17 +0600

    done carrer

--- src/pages/Career/Career.jsx
+++ src/pages/Career/Career.jsx
@@ -21,21 +21,24 @@ const Career = () => {
                   />
                 </div>
                 <div className="w-full md:w-1/2 flex justify-center items-center p-4">
-        <div className="text-center">
-          <h1 className="text-5xl font-bold mb-4">
-            Be a Part of The Fastest Growing Talented Team
-          </h1>
-          <p className="text-white mb-6">
-            Join our team as a software engineer and experience a dynamic work environment with opportunities for growth. Be part of a passionate and cohesive team delivering innovative software solutions.
-          </p>
-          <a
-            href="#"
-            className="inline-block bg-blue-500 text-white font-semibold py-2 px-4 rounded hover:bg-blue-600"
-          >
-            Explore Job Openings &rarr;
-          </a>
-        </div>
-      </div>
+                  <div className="text-center">
+                    <h1 className="text-5xl font-bold mb-4">
+                      Be a Part of The Fastest Growing Talented Team
+                    </h1>
+                    <p className="text-white mb-6">
+                      Join our team as a software engineer and experience a
+                      dynamic work environment with opportunities for growth. Be
+                      part of a passionate and cohesive team delivering
+                      innovative software solutions.
+                    </p>
+                    <a
+                      href="#"
+                      className="inline-block bg-cyan-500 text-white font-semibold py-2 px-4 rounded hover:bg-white hover:text-cyan-500"
+                    >
+                      Explore Job Openings &rarr;
+                    </a>
+                  </div>
+                </div>
                 <div className="w-full md:w-1/4 p-4">
                   <img
                     className="rounded-tl-[80px] rounded-br-[80px] shadow-md"
