4bc1130bfc381a4105a90eed278e106252e6d1e0
Talha <mdtalha2008@gmail.com>
2024-07-02 21:19:30 +0600

    carrer bug fixed

---
+++ src/pages/Career/Career.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const Career = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Career;
