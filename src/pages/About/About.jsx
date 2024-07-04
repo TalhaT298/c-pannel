28084ccca3daf336c50f1c03909dbabc3906ca9f
Talha <mdtalha2008@gmail.com>
2024-07-05 03:32:07 +0600

    about add

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -6,6 +6,11 @@ import tal from "../../assets/tal.png";
 import { FaAngleDoubleRight } from "react-icons/fa";
 import { FaArrowRight } from "react-icons/fa";
 import { motion } from "framer-motion";
+import { FaFacebookF } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa6";
+import { FaWhatsapp } from "react-icons/fa";
+import { BiLogoGmail } from "react-icons/bi";
 const About = () => {
   return (
     <>
@@ -45,6 +50,238 @@ const About = () => {
         </div>
       </div>
       {/* 2nd phase finish */}
+      {/* 3rd phase Strat */}
+      <div className="flex flex-wrap -m-4">
+              <div className="xl:w-1/4 md:w-1/2 p-4">
+                <div className="bg-gray-100 p-6 rounded-lg">
+                  <img
+                    className="h-52 rounded w-full object-cover object-center mb-6"
+                    src=""
+                    alt="content"
+                  ></img>
+                  <h3 className=" text-black text-xs font-medium title-font">
+                    Lecturer,B.Sc. in CSE(RU)
+                  </h3>
+                  <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
+                    Md. Khalid Hossen
+                  </h2>
+                  <div className="grid grid-flow-col gap-3">
+                  <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                       
+                        <a href="https://www.facebook.com/khalid.hasan.77128261?mibextid=ZbWKwL">
+                          <FaFacebookF href="https://www.facebook.com/khalid.hasan.77128261?mibextid=ZbWKwL" size="1.7rem" color="black" />
+                        </a>
+                      
+                        </motion.div>
+
+                    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                      <Link to="">
+                        <a href="">
+                          <FaInstagram size="1.7rem" color="black" />
+                        </a>
+                      </Link>
+                    </motion.div>
+
+                    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                      <Link to="">
+                        <a href="">
+                          <FaLinkedinIn size="1.7rem" color="black" />
+                        </a>
+                      </Link>
+                    </motion.div>
+
+                    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                      <Link to="">
+                        <a href="">
+                          <FaWhatsapp size="1.7rem" color="black" />
+                        </a>
+                      </Link>
+                    </motion.div>
+                    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                      <Link to="">
+                        <a href="">
+                          <BiLogoGmail size="1.7rem" color="black" />
+                        </a>
+                      </Link>
+                    </motion.div>
+                  </div>
+                </div>
+              </div>
+              <div className="xl:w-1/4 md:w-1/2 p-4">
+                <div className="bg-gray-100 p-6 rounded-lg">
+                  <img
+                    className="h-52 rounded w-full object-cover object-center mb-6"
+                    src=""
+                    alt="content"
+                  ></img>
+                  <h3 className="tracking-widest text-black text-xs font-medium title-font">
+                    Lecturer,B.Sc. in CSE(AIUB)
+                  </h3>
+                  <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
+                    Dipanjan Das Dipto
+                  </h2>
+                  <div className="grid grid-flow-col gap-3">
+                    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                      <Link to="">
+                        <a href="https://www.facebook.com/Dipto93?mibextid=ZbWKwL">
+                          <FaFacebookF size="1.7rem" color="black" />
+                        </a>
+                      </Link>
+                    </motion.div>
+
+                    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                      <Link to="">
+                        <a href="">
+                          <FaInstagram size="1.7rem" color="black" />
+                        </a>
+                      </Link>
+                    </motion.div>
+
+                    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                      <Link to="">
+                        <a href="">
+                          <FaLinkedinIn size="1.7rem" color="black" />
+                        </a>
+                      </Link>
+                    </motion.div>
+
+                    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                      <Link to="">
+                        <a href="">
+                          <FaWhatsapp size="1.7rem" color="black" />
+                        </a>
+                      </Link>
+                    </motion.div>
+                    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                      <Link to="">
+                        <a href="">
+                          <BiLogoGmail size="1.7rem" color="black" />
+                        </a>
+                      </Link>
+                    </motion.div>
+                  </div>
+                </div>
+              </div>
+              <div className="xl:w-1/4 md:w-1/2 p-4">
+                <div className="bg-gray-100 p-6 rounded-lg">
+                  <img
+                    className="h-52 rounded w-full object-cover object-center mb-6"
+                    src=""
+                    alt="content"
+                  ></img>
+                  <h3 className="tracking-widest text-black text-xs font-medium title-font">
+                    Lecturer,B.Sc. in CSE(SUST)
+                  </h3>
+                  <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
+                    Jnananjan Roy
+                  </h2>
+                  <div className="grid grid-flow-col gap-3">
+                    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                      <Link to="">
+                        <a href="https://www.facebook.com/profile.php?id=100000580366774&mibextid=JRoKGi">
+                          <FaFacebookF size="1.7rem" color="black" />
+                        </a>
+                      </Link>
+                    </motion.div>
+
+                    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                      <Link to="">
+                        <a href="">
+                          <FaInstagram size="1.7rem" color="black" />
+                        </a>
+                      </Link>
+                    </motion.div>
+
+                    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                      <Link to="">
+                        <a href="">
+                          <FaLinkedinIn size="1.7rem" color="black" />
+                        </a>
+                      </Link>
+                    </motion.div>
+
+                    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                      <Link to="">
+                        <a href="">
+                          <FaWhatsapp size="1.7rem" color="black" />
+                        </a>
+                      </Link>
+                    </motion.div>
+                    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                      <Link to="">
+                        <a href="">
+                          <BiLogoGmail size="1.7rem" color="black" />
+                        </a>
+                      </Link>
+                    </motion.div>
+                  </div>
+                </div>
+              </div>
+              <div className="xl:w-1/4 md:w-1/2 p-4">
+                <div className="bg-gray-100 p-6 rounded-lg">
+                  <img
+                    className="h-52 rounded w-full object-cover object-center mb-6"
+                    src=""
+                    alt="content"
+                  ></img>
+                  <h3 className="tracking-widest text-black text-xs font-medium title-font">
+                    Lecturer,B.Sc. in CSE(RU)
+                  </h3>
+                  <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
+                    Kanij Rabeya Nancy
+                  </h2>
+                  <div className="grid grid-flow-col gap-3">
+                    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                      <Link to="https://www.facebook.com/profile.php?id=100012102336348">
+                        <a
+                          href="https://www.facebook.com/profile.php?id=100012102336348"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <FaFacebookF
+                            href="https://www.facebook.com/profile.php?id=100012102336348"
+                            size="1.7rem"
+                            color="black"
+                          />
+                        </a>
+                      </Link>
+                    </motion.div>
+
+                    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                      <Link to="">
+                        <a href="">
+                          <FaInstagram size="1.7rem" color="black" />
+                        </a>
+                      </Link>
+                    </motion.div>
+
+                    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                      <Link to="">
+                        <a href="">
+                          <FaLinkedinIn size="1.7rem" color="black" />
+                        </a>
+                      </Link>
+                    </motion.div>
+
+                    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                      <Link to="">
+                        <a href="">
+                          <FaWhatsapp size="1.7rem" color="black" />
+                        </a>
+                      </Link>
+                    </motion.div>
+                    <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+                      <Link to="">
+                        <a href="">
+                          <BiLogoGmail size="1.7rem" color="black" />
+                        </a>
+                      </Link>
+                    </motion.div>
+                  </div>
+                </div>
+              </div>
+            </div>
+      {/* 3rd phase finish */}
     </>
   );
 };
