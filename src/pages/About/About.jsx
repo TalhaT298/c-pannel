3303d49741b36116e419ea789348b45377dfda0b
Talha <mdtalha2008@gmail.com>
2024-07-05 02:08:03 +0600

    give opacity in image

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,19 +1,26 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import { Link } from 'react-router-dom';
-import background from "../../assets/back.png"
+import React from "react";
+import { Link } from "react-router-dom";
+import background from "../../assets/back.png";
 
 const About = () => {
-    return (
-        <>
-        <Link to="about">
-        <div>
-            <img src={background} alt="" />
+  return (
+    <>
+      <Link to="about">
+        <div className="relative w-full h-full">
+          <img
+            src={background}
+            alt="background"
+            className="w-full h-full object-cover"
+          />
+          <div className="absolute inset-0 bg-black opacity-40"></div>
+          <div className="absolute bottom-0 left-0 w-full p-4 text-white text-3xl text-center">
+            Your text goes here
+          </div>
         </div>
-        </Link>
-        </>
-        
-    );
+      </Link>
+    </>
+  );
 };
 
 export default About;
