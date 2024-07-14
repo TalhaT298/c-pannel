fae00f28918085faa60c304b7b7d87620797537d
Talha <mdtalha2008@gmail.com>
2024-07-15 03:25:48 +0600

    services update

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { Link } from "react-router-dom";
+import ser from "../../assets/service.png"
 
 const Services = () => {
-    return (
-        <Link to="/services">
+  return (
+    <Link to="/services">
+      <>
+        {/* 1st phase start */}
         <div>
-            
-            </div>
-        </Link>
-    );
+          <img src={ser} alt="" />
+        </div>
+        {/* 1st phase finish */}
+      </>
+    </Link>
+  );
 };
 
 export default Services;
