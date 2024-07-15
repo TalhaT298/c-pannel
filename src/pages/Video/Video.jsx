4bef9f5a41d2ca4bb776af0995447eb3834e84af
Talha <mdtalha2008@gmail.com>
2024-07-16 01:11:04 +0600

    video section add

---
+++ src/pages/Video/Video.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Video = () => {
+    return (
+        <>
+        <Link to="/video"></Link>
+        <div>
+            
+        </div>
+        </>
+    );
+};
+
+export default Video;
