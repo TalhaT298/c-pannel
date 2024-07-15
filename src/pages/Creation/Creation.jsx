ee28d1a3817a5bd3f5bd517db60dc6c81b51eb4b
Talha <mdtalha2008@gmail.com>
2024-07-16 01:09:23 +0600

    creation add

---
+++ src/pages/Creation/Creation.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Creation = () => {
+    return (
+        <>
+        <Link to="/creation"></Link>
+        <div>
+            
+        </div>
+        </>
+    );
+};
+
+export default Creation;
