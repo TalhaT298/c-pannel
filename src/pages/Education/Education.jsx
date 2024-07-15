25094e7df21fc5400ab72fcb171183d47bcbe9b8
Talha <mdtalha2008@gmail.com>
2024-07-16 01:07:48 +0600

    education add

---
+++ src/pages/Education/Education.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Education = () => {
+    return (
+        <>
+        <Link to="/education"></Link>
+        <div>
+            
+        </div>
+        </>
+    );
+};
+
+export default Education;
