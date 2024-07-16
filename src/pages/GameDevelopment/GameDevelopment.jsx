16a2b483d0baafce8cc1020012e85e55032e6760
Talha <mdtalha2008@gmail.com>
2024-07-16 13:51:54 +0600

    banner and button add

--- src/pages/GameDevelopment/GameDevelopment.jsx
+++ src/pages/GameDevelopment/GameDevelopment.jsx
@@ -1,7 +1,8 @@
+/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable no-unused-vars */
 import React from "react";
 import { Link } from "react-router-dom";
-
+import { PiArrowRightLight } from "react-icons/pi";
 const GameDevelopment = () => {
   return (
     <>
@@ -10,22 +11,32 @@ const GameDevelopment = () => {
         {/* ---------------------------------1st phase ----------------------------*/}
         <div className="flex flex-col md:flex-row items-center justify-between p-8 shadow-md rounded-lg">
           <div className="text-center md:text-left md:w-1/2 mb-8 md:mb-0">
-          <h3 className="text-4xl bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-800 text-transparent bg-clip-text uppercase pt-4 font-bold font-serif">
-          WordPress Development
-        </h3>
-            <h2 className="text-gray-800 font-extrabold text-2xl mb-4">
-            Register by contacting us
+            <h3 className="text-4xl bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-800 text-transparent bg-clip-text uppercase pt-4 font-bold font-serif">
+              WordPress Development
+            </h3>
+            <h2 className="text-white font-extrabold text-2xl mb-4">
+              Register by contacting us
             </h2>
-            <p className="text-gray-600 mb-4">
-              Welcome to Olden Goods! a Open buying and selling platform,
-            </p>
             <p className="text-gray-600 mb-8">
-              Unlock exclusive deals and personalized shopping. Register now for
-              a seamless online shopping experience for all!
+              Oceancapp's WordPress services are unmatched in their ability to
+              create customized solutions that cater specifically to our
+              clients' needs. We specialize in developing bespoke WordPress
+              themes and plugins, tailored to enhance functionality and meet
+              unique business requirements effectively. Our team excels in
+              providing comprehensive support, from initial design and
+              development to ongoing maintenance and updates, ensuring seamless
+              integration and optimal performance.We prioritize user-friendly
+              interfaces and robust security measures.
             </p>
-            <button className="bg-green-500 text-white font-bold py-2 px-4 rounded-full hover:bg-green-600 transition duration-300">
-              Register Now
-            </button>
+            <Link to="">
+              <button className="flex items-center gap-4 px-8 py-3 bg-gradient-to-t from-cyan-600 to-black text-white text-lg font-medium rounded-lg">
+                Learn More
+                <PiArrowRightLight
+                  size="1.1rem"
+                  className="text-white group-hover:text-black"
+                />
+              </button>
+            </Link>
           </div>
           <div className="md:w-1/2 flex items-center justify-center">
             <div className="relative">
