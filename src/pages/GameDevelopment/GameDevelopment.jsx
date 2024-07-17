cc9ef5d5968f783c4c73731d7c719f590a7f4d34
Talha <mdtalha2008@gmail.com>
2024-07-18 02:10:47 +0600

    showing skill add

--- src/pages/GameDevelopment/GameDevelopment.jsx
+++ src/pages/GameDevelopment/GameDevelopment.jsx
@@ -5,7 +5,12 @@ import { Link } from "react-router-dom";
 import { PiArrowRightLight } from "react-icons/pi";
 import word1 from "../../assets/manw.png";
 import word2 from "../../assets/word11.png";
+import { GrProjects } from "react-icons/gr";
 import sampleVideo from "../../assets/sample-video.mp4";
+import person1 from "../../assets/p1.png";
+import person2 from "../../assets/p2.png";
+import person3 from "../../assets/p3.png";
+import person4 from "../../assets/p4.png";
 const GameDevelopment = () => {
   return (
     <>
@@ -121,6 +126,103 @@ const GameDevelopment = () => {
           <p>Whether you're looking to build a new WordPress site or enhance your existing one, Oceancapp is here to help. Contact us today to discuss your project and discover how we can transform your vision into a seamless WordPress experience.</p>
         </p>
         {/* ---------------------------------3rd phase finish----------------------------*/}
+        {/* ---------------------------------4th phase start----------------------------*/}
+        <div className="flex items-center justify-center border border-black w-full h-[80vh] bg-gradient-to-tr from-black via-black/90 to-black">
+        <div className="flex flex-col items-center my-10 text-white space-y-8">
+          {/* featured div  */}
+          <div className="flex items-center gap-4 bg-gradient-to-t from-cyan-600 to-white/10 py-2 px-6 rounded-lg">
+            <div className=" w-8 h-8 text-black text-2xl font-semibold bg-white flex justify-center items-center rounded-full">
+              O
+            </div>
+            <div>
+              <p className="text-white text-sm">Featured on</p>
+              <p className="text-white font-medium">Ocean Hunt</p>
+            </div>
+          </div>
+          {/* header  */}
+          <h1 className="text-5xl font-semibold text-white text-center leading-[65px] ">
+            Show Us Your Superpower
+          </h1>
+          {/* description  */}
+          <p className="max-w-[58%] mx-auto text-center">
+            Turn your ideas into amazing plans effortlessly with
+            <span className="text-cyan-600"> Oceancapp. </span>
+          </p>
+          {/* people who use navigateUI  */}
+          <div className="flex items-center py-4 pl-4 bg-gradient-to-tr from-black via-white/10 to-black border border-white/10 rounded-xl">
+            <div className="w-14 h-14 bg-transparent  rounded-full border-[3px] border-[#191919]">
+              <img
+                className="w-full h-full rounded-full"
+                src={person1}
+                alt="avatar navigate ui"
+              />
+            </div>
+            <div className="w-14 h-14 bg-[#191919]  rounded-full border-[3px] border-[#191919] relative -left-5">
+              <img
+                className="w-full h-full rounded-full"
+                src={person2}
+                alt="avatar navigate ui"
+              />
+            </div>
+            <div className="w-14 h-14 bg-[#191919]  rounded-full border-[3px] border-[#191919]  relative -left-10">
+              <img
+                className="w-full h-full rounded-full"
+                src={person3}
+                alt="avatar navigate ui"
+              />
+            </div>
+            <div className="w-14 h-14 bg-[#191919]  rounded-full border-[3px] border-[#191919]  relative -left-[60px]">
+              <img
+                className="w-full h-full rounded-full"
+                src={person4}
+                alt="avatar navigate ui"
+              />
+            </div>
+
+            <div className="relative -left-5">
+              <p className="text-lg text-white">20+</p>
+              <p className="text-sm text-gray-400">people already showing</p>
+            </div>
+          </div>
+          {/* buttons  */}
+          <div className="flex flex-wrap gap-10 items-center py-4">
+            <Link to="">
+              <button className="flex items-center gap-4 px-8 py-3 bg-gradient-to-b from-cyan-600 to-black text-white text-lg font-medium rounded-lg">
+                Show Here{" "}
+                <svg
+                  width={25}
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g strokeWidth="0"></g>
+                  <g
+                    id="SVGRepo_tracerCarrier"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  ></g>
+                  <g id="SVGRepo_iconCarrier">
+                    {" "}
+                    <path
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M12.2929 4.29289C12.6834 3.90237 13.3166 3.90237 13.7071 4.29289L20.7071 11.2929C21.0976 11.6834 21.0976 12.3166 20.7071 12.7071L13.7071 19.7071C13.3166 20.0976 12.6834 20.0976 12.2929 19.7071C11.9024 19.3166 11.9024 18.6834 12.2929 18.2929L17.5858 13H4C3.44772 13 3 12.5523 3 12C3 11.4477 3.44772 11 4 11H17.5858L12.2929 5.70711C11.9024 5.31658 11.9024 4.68342 12.2929 4.29289Z"
+                      fill="white"
+                    ></path>
+                  </g>
+                </svg>
+              </button>
+            </Link>
+            <Link to="">
+              <button className="flex items-center gap-4 px-8 py-3 bg-gradient-to-t from-black to-white/10 text-white text-lg font-medium rounded-lg">
+                Projects <GrProjects className="" />
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+        {/* ---------------------------------4th phase finish----------------------------*/}
+
       </div>
     </>
   );
