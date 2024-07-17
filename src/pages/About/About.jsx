abe0599f1ab7dc46751c256f9cdd02cc7ebb0600
Talha <mdtalha2008@gmail.com>
2024-07-18 01:57:28 +0600

    brif add at oceancapp wordpress

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -200,34 +200,39 @@ const About = () => {
       </div>
       {/* 1st phase done finish*/}
       {/* 2nd phase 1 start joldi  ekhane*/}
-      <h3 className="text-4xl text-cyan-500 uppercase font-bold font-serif text-center pb-4">
+      <h3 className="text-4xl bg-gradient-to-r from-cyan-100 via-cyan-500 to-cyan-800 text-transparent bg-clip-text uppercase font-bold font-serif text-center pb-4">
         Our Journey
       </h3>
       <div className="h-1 w-40 md:w-80 bg-cyan-500 rounded flex justify-center items-center mx-auto my-auto hover:animate-ping "></div>
       <div>
         <p className="text-center font-semibold text-2xl pt-4">
-          <p>Welcome to Oceancapp, where innovation converges with expertise to
-          elevate your digital journey. As pioneers in the IT realm, we proudly
-          offer a comprehensive spectrum of services tailored to meet every
-          digital need imaginable. From sleek web development that captivates to
-          dynamic graphics design that mesmerizes, from bespoke WordPress
-          solutions that empower to immersive game development that thrills – we
-          do it all, and we do it with passion. </p><br />
-          <p> What sets Oceancapp apart is our
-          commitment to revolutionizing how IT solutions are delivered in
-          Bangladesh. By consolidating web development, graphics design,
-          WordPress customization, game development, prompt engineering, and
-          cybersecurity under one roof, we simplify complexity and amplify
-          efficiency for our clients.</p> 
-          <br /> Imagine a future where launching your
-          online presence or securing your digital assets is not only seamless
-          but also exhilarating. At Oceancapp, we don't just meet expectations;
-          we exceed them with our dedication to excellence, creativity, and
-          cutting-edge technology. Join us on this transformative journey as we
-          empower businesses, entrepreneurs, and visionaries across Bangladesh
-          to thrive in the digital age. Together, let's shape a brighter
-          tomorrow where innovation knows no bounds – powered by Oceancapp,
-          where your aspirations meet our expertise!
+          <p>
+            Welcome to Oceancapp, where innovation converges with expertise to
+            elevate your digital journey. As pioneers in the IT realm, we
+            proudly offer a comprehensive spectrum of services tailored to meet
+            every digital need imaginable. From sleek web development that
+            captivates to dynamic graphics design that mesmerizes, from bespoke
+            WordPress solutions that empower to immersive game development that
+            thrills – we do it all, and we do it with passion.{" "}
+          </p>
+          <br />
+          <p>
+            {" "}
+            What sets Oceancapp apart is our commitment to revolutionizing how
+            IT solutions are delivered in Bangladesh. By consolidating web
+            development, graphics design, WordPress customization, game
+            development, prompt engineering, and cybersecurity under one roof,
+            we simplify complexity and amplify efficiency for our clients.
+          </p>
+          <br /> Imagine a future where launching your online presence or
+          securing your digital assets is not only seamless but also
+          exhilarating. At Oceancapp, we don't just meet expectations; we exceed
+          them with our dedication to excellence, creativity, and cutting-edge
+          technology. Join us on this transformative journey as we empower
+          businesses, entrepreneurs, and visionaries across Bangladesh to thrive
+          in the digital age. Together, let's shape a brighter tomorrow where
+          innovation knows no bounds – powered by Oceancapp, where your
+          aspirations meet our expertise!
         </p>
       </div>
       {/* 2nd phase 1 start joldi  sesh eakhen*/}
@@ -280,60 +285,99 @@ const About = () => {
       </div> */}
       {/* 2nd pase part 1 start ----------------------------------------------*/}
       <div className="flex items-center justify-center border border-black w-full h-[80vh] bg-gradient-to-tr from-black via-black/90 to-black">
-            <div className="flex flex-col items-center my-10 text-white space-y-8">
-                {/* featured div  */}
-                <div className="flex items-center gap-4 bg-gradient-to-t from-cyan-600 to-white/10 py-2 px-6 rounded-lg">
-                    <div className=" w-8 h-8 text-black text-2xl font-semibold bg-white flex justify-center items-center rounded-full">O</div>
-                    <div>
-                        <p className="text-white text-sm">Featured on</p>
-                        <p className="text-white font-medium">Ocean Hunt</p>
-                    </div>
-                </div>
-                {/* header  */}
-                <h1 className="text-5xl font-semibold text-white text-center leading-[65px] ">
-                Show Us Your Superpower 
-                </h1>
-                {/* description  */}
-                <p className="max-w-[58%] mx-auto text-center">
-                    Turn your ideas into amazing plans effortlessly with<span className="text-cyan-600"> Oceancapp. </span>
-                </p>
-                {/* people who use navigateUI  */}
-                <div className="flex items-center py-4 pl-4 bg-gradient-to-tr from-black via-white/10 to-black border border-white/10 rounded-xl">
-                    <div className="w-14 h-14 bg-transparent  rounded-full border-[3px] border-[#191919]">
-                        <img className="w-full h-full rounded-full" src={person1} alt="avatar navigate ui" />
-                    </div>
-                    <div className="w-14 h-14 bg-[#191919]  rounded-full border-[3px] border-[#191919] relative -left-5">
-                        <img className="w-full h-full rounded-full" src={person2} alt="avatar navigate ui" />
-                    </div>
-                    <div className="w-14 h-14 bg-[#191919]  rounded-full border-[3px] border-[#191919]  relative -left-10">
-                        <img className="w-full h-full rounded-full" src={person3} alt="avatar navigate ui" />
-                    </div>
-                    <div className="w-14 h-14 bg-[#191919]  rounded-full border-[3px] border-[#191919]  relative -left-[60px]">
-                        <img className="w-full h-full rounded-full" src={person4} alt="avatar navigate ui" />
-                    </div>
-
-                    <div className="relative -left-5">
-                        <p className="text-lg text-white">420+</p>
-                        <p className="text-sm text-gray-400">people already showing</p>
-                    </div>
-                </div>
-                {/* buttons  */}
-                <div className="flex flex-wrap gap-10 items-center py-4">
-                    <Link to="">
-                    <button className="flex items-center gap-4 px-8 py-3 bg-gradient-to-b from-cyan-600 to-black text-white text-lg font-medium rounded-lg">
-                        Show Here{' '}<svg width={25} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier">                              <path fillRule="evenodd" clipRule="evenodd" d="M12.2929 4.29289C12.6834 3.90237 13.3166 3.90237 13.7071 4.29289L20.7071 11.2929C21.0976 11.6834 21.0976 12.3166 20.7071 12.7071L13.7071 19.7071C13.3166 20.0976 12.6834 20.0976 12.2929 19.7071C11.9024 19.3166 11.9024 18.6834 12.2929 18.2929L17.5858 13H4C3.44772 13 3 12.5523 3 12C3 11.4477 3.44772 11 4 11H17.5858L12.2929 5.70711C11.9024 5.31658 11.9024 4.68342 12.2929 4.29289Z" fill="white"></path></g></svg>
-                    </button>
-                    </Link>
-                    <Link to="">
-                    <button className="flex items-center gap-4 px-8 py-3 bg-gradient-to-t from-black to-white/10 text-white text-lg font-medium rounded-lg">
-                       
-                        Projects <GrProjects className=""/>
-                       
-                    </button>
-                    </Link>
-                </div>
+        <div className="flex flex-col items-center my-10 text-white space-y-8">
+          {/* featured div  */}
+          <div className="flex items-center gap-4 bg-gradient-to-t from-cyan-600 to-white/10 py-2 px-6 rounded-lg">
+            <div className=" w-8 h-8 text-black text-2xl font-semibold bg-white flex justify-center items-center rounded-full">
+              O
             </div>
+            <div>
+              <p className="text-white text-sm">Featured on</p>
+              <p className="text-white font-medium">Ocean Hunt</p>
+            </div>
+          </div>
+          {/* header  */}
+          <h1 className="text-5xl font-semibold text-white text-center leading-[65px] ">
+            Show Us Your Superpower
+          </h1>
+          {/* description  */}
+          <p className="max-w-[58%] mx-auto text-center">
+            Turn your ideas into amazing plans effortlessly with
+            <span className="text-cyan-600"> Oceancapp. </span>
+          </p>
+          {/* people who use navigateUI  */}
+          <div className="flex items-center py-4 pl-4 bg-gradient-to-tr from-black via-white/10 to-black border border-white/10 rounded-xl">
+            <div className="w-14 h-14 bg-transparent  rounded-full border-[3px] border-[#191919]">
+              <img
+                className="w-full h-full rounded-full"
+                src={person1}
+                alt="avatar navigate ui"
+              />
+            </div>
+            <div className="w-14 h-14 bg-[#191919]  rounded-full border-[3px] border-[#191919] relative -left-5">
+              <img
+                className="w-full h-full rounded-full"
+                src={person2}
+                alt="avatar navigate ui"
+              />
+            </div>
+            <div className="w-14 h-14 bg-[#191919]  rounded-full border-[3px] border-[#191919]  relative -left-10">
+              <img
+                className="w-full h-full rounded-full"
+                src={person3}
+                alt="avatar navigate ui"
+              />
+            </div>
+            <div className="w-14 h-14 bg-[#191919]  rounded-full border-[3px] border-[#191919]  relative -left-[60px]">
+              <img
+                className="w-full h-full rounded-full"
+                src={person4}
+                alt="avatar navigate ui"
+              />
+            </div>
+
+            <div className="relative -left-5">
+              <p className="text-lg text-white">420+</p>
+              <p className="text-sm text-gray-400">people already showing</p>
+            </div>
+          </div>
+          {/* buttons  */}
+          <div className="flex flex-wrap gap-10 items-center py-4">
+            <Link to="">
+              <button className="flex items-center gap-4 px-8 py-3 bg-gradient-to-b from-cyan-600 to-black text-white text-lg font-medium rounded-lg">
+                Show Here{" "}
+                <svg
+                  width={25}
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g strokeWidth="0"></g>
+                  <g
+                    id="SVGRepo_tracerCarrier"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  ></g>
+                  <g id="SVGRepo_iconCarrier">
+                    {" "}
+                    <path
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M12.2929 4.29289C12.6834 3.90237 13.3166 3.90237 13.7071 4.29289L20.7071 11.2929C21.0976 11.6834 21.0976 12.3166 20.7071 12.7071L13.7071 19.7071C13.3166 20.0976 12.6834 20.0976 12.2929 19.7071C11.9024 19.3166 11.9024 18.6834 12.2929 18.2929L17.5858 13H4C3.44772 13 3 12.5523 3 12C3 11.4477 3.44772 11 4 11H17.5858L12.2929 5.70711C11.9024 5.31658 11.9024 4.68342 12.2929 4.29289Z"
+                      fill="white"
+                    ></path>
+                  </g>
+                </svg>
+              </button>
+            </Link>
+            <Link to="">
+              <button className="flex items-center gap-4 px-8 py-3 bg-gradient-to-t from-black to-white/10 text-white text-lg font-medium rounded-lg">
+                Projects <GrProjects className="" />
+              </button>
+            </Link>
+          </div>
         </div>
+      </div>
       {/* 2nd pase part 1------------------------------------- finish */}
       {/* 2nd phase finish */}
       {/* 3rd phase Strat */}
@@ -524,7 +568,10 @@ const About = () => {
                   <FaWhatsapp size="1.7rem" color="black" />
                 </a>
               </motion.div>
-              <motion.div href="mailto:thdip128@gmail.com" whileTap={{ rotate: 90, scale: 0.75 }}>
+              <motion.div
+                href="mailto:thdip128@gmail.com"
+                whileTap={{ rotate: 90, scale: 0.75 }}
+              >
                 <a href="mailto:thdip128@gmail.com">
                   <BiLogoGmail
                     href="mailto:thdip128@gmail.com"
--- src/pages/GameDevelopment/GameDevelopment.jsx
+++ src/pages/GameDevelopment/GameDevelopment.jsx
@@ -3,8 +3,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { PiArrowRightLight } from "react-icons/pi";
-import word1 from "../../assets/manw.png"
-import word2 from "../../assets/word11.png"
+import word1 from "../../assets/manw.png";
+import word2 from "../../assets/word11.png";
 import sampleVideo from "../../assets/sample-video.mp4";
 const GameDevelopment = () => {
   return (
@@ -60,7 +60,10 @@ const GameDevelopment = () => {
         {/* ---------------------------------2nd phase start----------------------------*/}
         <div className="p-8 mt-8 rounded-lg shadow-md">
           <div className="flex flex-col md:flex-row items-center justify-between">
-            <video controls className="w-full md:w-1/2 rounded-lg shadow-lg mb-4 md:mb-0">
+            <video
+              controls
+              className="w-full md:w-1/2 rounded-lg shadow-lg mb-4 md:mb-0"
+            >
               <source src={sampleVideo} type="video/mp4" />
               Your browser does not support the video tag.
             </video>
@@ -72,15 +75,15 @@ const GameDevelopment = () => {
                 Watch this introductory video to get a better understanding of
                 our comprehensive range of services. Our team of experts is
                 dedicated to providing top-notch solutions tailored to meet your
-                specific needs. From initial consultation to final implementation,
-                we ensure a seamless experience.
+                specific needs. From initial consultation to final
+                implementation, we ensure a seamless experience.
               </p>
               <a
                 href={sampleVideo}
                 download
                 className="flex items-center gap-4 px-8 py-3 bg-gradient-to-t from-cyan-600 to-black text-white text-lg font-medium rounded-lg"
               >
-                Download our intro video 
+                Download our intro video
                 <PiArrowRightLight
                   size="1.1rem"
                   className="text-white group-hover:text-black"
@@ -90,7 +93,34 @@ const GameDevelopment = () => {
           </div>
         </div>
         {/* ---------------------------------2nd phase finish----------------------------*/}
-
+        {/* ---------------------------------3rd phase start----------------------------*/}
+        <div>
+          <h3 className="text-4xl py-4 text-center bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-800 text-transparent bg-clip-text uppercase pt-4 font-bold font-serif">
+            WordPress Excellence Delivered
+          </h3>
+        </div>
+        <div className="h-1 w-80 bg-cyan-600 rounded flex justify-center items-center mx-auto my-auto hover:animate-ping"></div>
+        <p className="text-center font-thin text-xl pt-4">
+          <p>At Oceancapp, we pride ourselves on offering unparalleled WordPress
+          development services that stand out in the industry. Our commitment to
+          quality, innovation, and customer satisfaction ensures that your
+          WordPress site will not only meet but exceed your expectations.</p> <br />
+          <p>Our team of skilled developers has extensive experience in creating
+          custom WordPress solutions tailored to your unique needs. We stay
+          updated with the latest trends and technologies to deliver
+          cutting-edge websites. We understand that every business is unique,
+          which is why we provide customized WordPress solutions that align with
+          your brand identity and business goals. From bespoke themes to
+          tailored plugins, we've got you covered.</p> <br />
+          <p>
+          Our focus is on creating websites that are not only visually appealing but also user-friendly. We ensure seamless navigation and an engaging user experience to keep your visitors coming back. Additionally, we build WordPress sites with SEO best practices in mind, helping to improve your website’s visibility on search engines and driving more organic traffic to your site.
+          </p><br />
+          <p>With the increasing use of mobile devices, having a responsive website is crucial. Our WordPress sites are optimized for all devices, ensuring a seamless experience for your users, whether they're on a desktop, tablet, or smartphone. We also prioritize the performance and security of your website. Our developers implement best practices to ensure your site loads quickly and remains secure against potential threats.</p><br />
+          <p>At Oceancapp, we believe in showcasing our expertise through our work. You can easily display your best WordPress sites directly on your website with our portfolio feature, highlighting your top projects and demonstrating your capabilities to potential clients and partners.</p><br />
+          <p>Are you passionate about WordPress development and looking to join a dynamic team? Oceancapp is always on the lookout for talented individuals to join our growing family. As part of our team, you'll have the opportunity to work on exciting projects, develop your skills, and contribute to our mission of delivering excellence in WordPress development.</p><br />
+          <p>Whether you're looking to build a new WordPress site or enhance your existing one, Oceancapp is here to help. Contact us today to discuss your project and discover how we can transform your vision into a seamless WordPress experience.</p>
+        </p>
+        {/* ---------------------------------3rd phase finish----------------------------*/}
       </div>
     </>
   );
