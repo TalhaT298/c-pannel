9214123d4a10da1e4989d5ffd15d4642def70055
Talha <mdtalha2008@gmail.com>
2024-08-01 20:40:10 +0600

    headline remove

--- src/pages/WebDevelopment/WebDevelopment.jsx
+++ src/pages/WebDevelopment/WebDevelopment.jsx
@@ -336,7 +336,7 @@ const WebDevelopment = () => {
         <div className="bg-gray-900 text-white">
 
 <div className="container mx-auto py-8">
-  <h1 className="text-center text-3xl font-bold mb-8">For the Love of Reading</h1>
+  
 
   <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
     
