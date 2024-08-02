1cdeccd5f0735f7dccf24452513361404e1b1911
Talha <mdtalha2008@gmail.com>
2024-08-02 15:32:18 +0600

    contact add

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -114,13 +114,79 @@
 //2nd phase
 // export default Contact;
 
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import { Link } from 'react-router-dom';
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_ryk1ydk', 'template_8306npz', form.current, 'Y8-44Z6hmp2exw81g')
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
   return (
     <div>
        <Link to="/contact"></Link>
+       <div className='py-10 flex justify-center items-center'>
+      <div className="w-full max-w-3xl rounded-lg bg-white px-10 pb-10 pt-8 shadow-md dark:bg-cyan-700">
+        <div className="mb-6">
+          <h2 className="text-center text-white text-3xl font-semibold tracking-tight">Contact Us</h2>
+        </div>
+        <form ref={form} onSubmit={sendEmail} className="w-full space-y-6">
+          <div className="space-y-2 text-sm text-zinc-700 dark:text-zinc-400">
+            <label className="block font-medium" htmlFor="user_name">
+              Name
+            </label>
+            <input
+              className="h-10 w-full rounded border px-3 py-2 text-sm leading-tight focus:outline-none focus:ring-1 dark:border-zinc-700"
+              id="user_name"
+              placeholder="Your Name"
+              name="user_name"
+              type="text"
+            />
+          </div>
+          <div className="space-y-2 text-sm text-zinc-700 dark:text-zinc-400">
+            <label className="block font-medium" htmlFor="user_email">
+              Email
+            </label>
+            <input
+              className="h-10 w-full rounded border px-3 py-2 text-sm leading-tight focus:outline-none focus:ring-1 dark:border-zinc-700"
+              id="user_email"
+              placeholder="Your Email"
+              name="user_email"
+              type="email"
+            />
+          </div>
+          <div className="space-y-2 text-sm text-zinc-700 dark:text-zinc-400">
+            <label className="block font-medium" htmlFor="message">
+              Message
+            </label>
+            <textarea
+              className="min-h-[80px] w-full rounded border px-3 py-2 leading-tight focus:outline-none focus:ring-1 dark:border-zinc-700"
+              id="message"
+              placeholder="What's on your mind"
+              name="message"
+            />
+          </div>
+          <button className="rounded-md bg-sky-500 px-4 py-2 text-teal-600 transition-colors dark:bg-white" type="submit">
+            Submit
+          </button>
+        </form>
+        {/* done */}
+      </div>
+    </div>
     </div>
   );
 };
