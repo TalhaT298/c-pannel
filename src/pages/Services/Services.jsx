e748bfa54512de94b695a4409af0fcac14e39634
Talha <mdtalha2008@gmail.com>
2024-08-02 19:47:51 +0600

    servive update

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -31,11 +31,11 @@ const Services = () => {
         <div>
           <div className="mx-auto md:w-8/12 mb-2 mt-16 flex justify-center items-center ">
             {/* <p className='text-yellow-600'>----{subHeading}--</p */}
-            <h3 className="text-4xl bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-800 text-transparent bg-clip-text uppercase pt-4 font-bold font-serif">
+            <h3 className="text-2xl sm:text-3xl md:text-4xl bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-800 text-transparent bg-clip-text uppercase pt-4 font-bold font-serif">
               OUR SERVICES
             </h3>
           </div>
-          <div className="h-1 w-80 bg-cyan-600 rounded flex justify-center items-center mx-auto my-auto hover:animate-ping"></div>
+          <div className="h-1 w-10 bg-cyan-600 rounded flex justify-center items-center mx-auto my-auto hover:animate-ping"></div>
           <p className="text-center py-4 font-thin">
             Oceancapp offers a diverse range of IT solutions to meet various
             needs. Our web development services include custom website design,
@@ -615,11 +615,11 @@ const Services = () => {
         {/* 11th phase finish----------------------------------- */}
         {/* 12th phase start----------------------------------- */}
         <div className="mx-auto md:w-8/12 mb-2 mt-10 flex justify-center items-center ">
-          <h3 className="text-4xl bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-800 text-transparent bg-clip-text uppercase pt-4 font-bold font-serif">
+          <h3 className="text-2xl sm:text-3xl md:text-4xl bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-800 text-transparent bg-clip-text uppercase pt-4 font-bold font-serif">
             Words From Clients
           </h3>
         </div>
-        <div className="h-1 w-80 bg-cyan-600 rounded flex justify-center items-center mx-auto my-auto hover:animate-ping"></div>
+        <div className="h-1 w-10 bg-cyan-600 rounded flex justify-center items-center mx-auto my-auto hover:animate-ping"></div>
         <div>
           <section class="text-gray-600 body-font overflow-hidden">
             <div className="container px-5 py-12 mx-auto">
