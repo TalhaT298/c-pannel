ebffa11740f09ab40a7051a786ae0926a2488419
Talha <mdtalha2008@gmail.com>
2024-08-07 03:35:50 +0600

    idea update

--- src/pages/Home/Idea/Idea.jsx
+++ src/pages/Home/Idea/Idea.jsx
@@ -6,8 +6,7 @@ import { Link } from "react-router-dom";
 import { PiArrowRightLight } from "react-icons/pi";
 const Idea = () => {
   return (
-    
-    <div className="flex flex-col md:flex-row">
+    <div className="flex flex-col md:flex-row py-4">
       <div className="w-full md:w-3/5 pt-6 lg:pl-20 lg:pt-24 pl-4 pr-4">
         <h1 className="lg:text-4xl text-xl font-bold lg:pb-6 pb-3">
           Have Any Project Idea
@@ -17,17 +16,15 @@ const Idea = () => {
           Global team. <br />
           You can schedule a free consultation.
         </p>
-        <Link to="https://www.facebook.com/oceancapp">
-        <button className=" text-xl pl-1 mt-6 w- bg-black text-white border-black border group hover:bg-black hover:text-white">
-          
-          <a
-            href="#"
-            className="flex items-center gap-4 px-8 py-3 bg-gradient-to-b from-cyan-600 to-black text-white text-lg font-medium rounded-lg"
-          >
-             Get Started &rarr;
-          </a>
-        </button>
-        
+        <Link to="https://wa.link/8bkntc">
+          <button className=" text-xl pl-1 mt-6">
+            <a
+              href="https://wa.link/8bkntc"
+              className="flex items-center gap-4 px-8 py-3 bg-gradient-to-b from-cyan-600 to-black text-white text-lg font-medium rounded-lg"
+            >
+              Get Started &rarr;
+            </a>
+          </button>
         </Link>
       </div>
       <div className="w-full md:w-2/5 lg:pl-20 pl-4 pr-4">
