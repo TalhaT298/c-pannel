4f58f1c0af8da5aef49fb60dac3106292507d7c9
Talha <mdtalha2008@gmail.com>
2024-08-07 19:10:39 +0600

    graphics add

--- src/pages/GraphicsDesign/GraphicsDesign.jsx
+++ src/pages/GraphicsDesign/GraphicsDesign.jsx
@@ -110,6 +110,7 @@ const GraphicsDesign = () => {
         </div>
         {/* ---------------------------------2nd phase finish----------------------------*/}
         {/* ---------------------------------3rd phase start----------------------------*/}
+        <div className="py-6">
         <div>
           <h3 className="text-2xl sm:text-3xl md:text-4xl py-4 text-center bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-800 text-transparent bg-clip-text uppercase pt-4 font-bold font-serif">
             graphic design Excellence Delivered
@@ -175,12 +176,13 @@ const GraphicsDesign = () => {
             creativity and impact to your projects.
           </p>
         </p>
+        </div>
         {/* ---------------------------------3rd phase finish----------------------------*/}
         {/* ---------------------------------4th phase start----------------------------*/}
         <div className="flex items-center justify-center border border-black w-full h-[80vh] bg-gradient-to-tr from-black via-black/90 to-black">
           <div className="flex flex-col items-center my-10 text-white space-y-8">
             {/* featured div  */}
-            <div className="flex items-center gap-4 bg-gradient-to-t from-cyan-600 to-white/10 py-2 px-6 rounded-lg">
+            <div className="flex items-center gap-4 bg-gradient-to-t from-cyan-600 to-white/10 py-2 px-6 my-6 rounded-lg">
               <div className=" w-8 h-8 text-black text-2xl font-semibold bg-white flex justify-center items-center rounded-full">
                 O
               </div>
