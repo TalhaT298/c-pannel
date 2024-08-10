bf6c54e45123c3341756b07cf7f32e952efcf142
Talha <mdtalha2008@gmail.com>
2024-08-11 00:40:15 +0600

    cyber update

--- src/pages/GameDevelopment/GameDevelopment.jsx
+++ src/pages/GameDevelopment/GameDevelopment.jsx
@@ -176,8 +176,8 @@ const GameDevelopment = () => {
         </p>
         {/* ---------------------------------3rd phase finish----------------------------*/}
         {/* ---------------------------------4th phase start----------------------------*/}
-        <div className="flex items-center justify-center border border-black w-full h-[80vh] bg-gradient-to-tr from-black via-black/90 to-black">
-          <div className="flex flex-col items-center my-10 text-white space-y-8">
+        <div className="flex items-center justify-center  w-full h-[80vh] bg-gradient-to-tr from-black via-black/90 to-black">
+          <div className="flex flex-col items-center my-10 mt-8 text-white space-y-8">
             {/* featured div  */}
             <div className="flex items-center gap-4 bg-gradient-to-t from-cyan-600 to-white/10 py-2 px-6 rounded-lg">
               <div className=" w-8 h-8 text-black text-2xl font-semibold bg-white flex justify-center items-center rounded-full">
@@ -272,6 +272,7 @@ const GameDevelopment = () => {
         </div>
         {/* ---------------------------------4th phase finish----------------------------*/}
         {/* ---------------------------------5th phase start----------------------------*/}
+        <div className="py-8">
         <Marquee className="pt-8" direction="right" speed={50}>
           <p className="text-xl md:text-4xl font-extrabold font-serif px-4">Themes</p>
           <p className="text-xl md:text-4xl font-extrabold font-serif px-4">Core</p>
@@ -321,6 +322,7 @@ const GameDevelopment = () => {
             Custom Post Types
           </p>
         </Marquee>
+        </div>
         {/* ---------------------------------5th phase finish----------------------------*/}
         {/* ---------------------------------6th phase finish----------------------------*/}
         <div>
