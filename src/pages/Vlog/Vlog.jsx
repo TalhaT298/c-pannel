c10b6a537535f39f6cb5c7d268c3bfb18a03c6f3
Talha <mdtalha2008@gmail.com>
2024-08-21 23:06:14 +0600

    vlog update

--- src/pages/Vlog/Vlog.jsx
+++ src/pages/Vlog/Vlog.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import gr from "../../../src/assets/gr.png";
-import { CiFaceSmile } from "react-icons/ci";
+import { FaArrowRight } from "react-icons/fa";
 const Vlog = () => {
   return (
     <>
@@ -20,8 +20,8 @@ const Vlog = () => {
         {/* -----------------1st phase start -------------------------*/}
         <div className="relative bg-gray-900 p-6 rounded-lg shadow-lg">
           <div className="absolute top-4 right-4">
-            <button className="w-10 h-10 flex items-center justify-center rounded-full bg-gray-700 hover:bg-gray-600">
-              <CiFaceSmile className="w-5 h-5" />
+            <button className="w-16 h-16 flex items-center justify-center rounded-full bg-gray-700 hover:bg-gray-600">
+              <FaArrowRight className="w-5 h-5" />
             </button>
           </div>
 
@@ -35,13 +35,13 @@ const Vlog = () => {
             </div>
 
             <div className="w-1/2 pl-6 text-gray-300 mt-96">
-              <p className="text-sm text-gray-400">August 13, 2024</p>
+              <p className="text-sm text-gray-200">August 13, 2024</p>
 
-              <h2 className="text-2xl font-bold text-white mt-2">
+              <h2 className="text-3xl font-bold text-white mt-2">
                 Grok-2 Beta Release
               </h2>
 
-              <p className="text-gray-400 mt-4">
+              <p className="text-gray-200 mt-4">
                 Grok-2 is our frontier language model with state-of-the-art
                 reasoning capabilities. This release includes two members of the
                 Grok family: Grok-2 and Grok-2 mini. Both models are now being
